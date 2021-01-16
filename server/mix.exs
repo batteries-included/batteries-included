85f0616964d2f2df97fb7c7469f074e1f81a3cfe
Elliott Clark <elliott.neil.clark@gmail.com>
2021-01-16 06:02:32 +0000

    Add phx_gen_auth

--- server/mix.exs
+++ server/mix.exs
@@ -46,7 +46,10 @@ defmodule Server.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+
+      # Auth
+      {:phx_gen_auth, "~> 0.6", only: [:dev], runtime: false}
     ]
   end
 
