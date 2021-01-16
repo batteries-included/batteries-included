45b173d9449778d4b0e5baacdb4dbb091f228021
Elliott Clark <elliott.neil.clark@gmail.com>
2021-01-16 07:45:47 +0000

    Bootstrap bash script for a while until we can self host.

---
+++ dev/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
+
+set -x
+# Grab the location we'll use it for yaml locations soon
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Create the cluster
+# kind create cluster --config "${DIR}/kind-config.yaml"
+k3d cluster create || true
+# Start the services.
+kubectl apply -f "${DIR}/k8s"
+
+# sleep some amount of time while waiting. This is horrible.
+# Yeah
+# so what.
+# I'm sure there's a better kubectl way but this is a hack while we can't self host.
+sleep 90
+
+kubectl port-forward svc/postgres 5432:5432
--- server/config/dev.exs
+++ server/config/dev.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 # Configure your database
 config :server, Server.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "server_dev",
+  username: "batterydbuser",
+  password: "batterypasswd",
+  database: "bi_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
