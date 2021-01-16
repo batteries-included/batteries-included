d7e2f962cef0bb1fd617d06c3c0781b24a36f1a4
Elliott Clark <elliott.neil.clark@gmail.com>
2021-01-16 06:00:41 +0000

    Add the phoenix generated server

---
+++ server/config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :server,
+  ecto_repos: [Server.Repo]
+
+# Configures the endpoint
+config :server, ServerWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "s3EZzlHQ8TGyfQYw+LgNkYEHKaKjtmLnc3aQh2+eUyfRa0UuE1Yf44hI1jWk0ii3",
+  render_errors: [view: ServerWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Server.PubSub,
+  live_view: [signing_salt: "YG8AIjsI"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
