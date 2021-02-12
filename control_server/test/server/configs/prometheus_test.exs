1f7fa64cf6809f1215ce8470b727f58f493440db
Elliott Clark <elliott.neil.clark@gmail.com>
2021-02-12 21:34:31 +0000

    Add on test for Prometheus.base_config!

--- control_server/test/server/configs/prometheus_test.exs
+++ control_server/test/server/configs/prometheus_test.exs
@@ -12,4 +12,13 @@ defmodule Server.Configs.PrometheusTest do
     {:ok, %RawConfig{} = raw_config} = Prometheus.create_for_cluster(cluster_id)
     assert raw_config.path == "/prometheus/base"
   end
+
+  test "base_config" do
+    cluster = insert(:kube_cluster)
+    cluster_id = cluster.id
+    Prometheus.create_for_cluster(cluster_id)
+
+    raw_config = Prometheus.base_config!(cluster_id)
+    assert raw_config.path == "/prometheus/base"
+  end
 end
