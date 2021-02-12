5e72afe56b2563bf566c1a85ef8ff96074c64b04
Elliott Clark <elliott.neil.clark@gmail.com>
2021-02-12 01:25:29 +0000

    Fix installing the basics of promettheus

--- operator/controller_core/src/prometheus.rs
+++ operator/controller_core/src/prometheus.rs
@@ -1,12 +1,23 @@
 use std::collections::BTreeMap;
 
-use common::error::Result;
-use k8s_openapi::api::{apps::v1::Deployment, core::v1::Service};
+use common::{cluster_spec::DEFAULT_NAMESPACE, error::Result};
+use k8s_openapi::api::{
+    apps::v1::Deployment,
+    core::v1::{ConfigMap, Service},
+};
 use serde_json::json;
+
+use kube::{
+    api::{Patch, PatchParams},
+    client::Client as KubeClient,
+    Api,
+};
 
 pub struct PrometheusInstaller {
     pub base_labels: BTreeMap<String, String>,
-    prometheus_labels: BTreeMap<String, String>,
+    deployment_name: String,
+    service_name: String,
+    config_map_name: String,
 }
 
 impl Default for PrometheusInstaller {
@@ -18,32 +29,63 @@ impl Default for PrometheusInstaller {
 impl PrometheusInstaller {
     #[must_use]
     pub fn new() -> Self {
-        let base_labels: BTreeMap<String, String> =
-            vec![("purpose".to_string(), "general-prometheus".to_string())]
-                .into_iter()
-                .collect();
-        let mut prometheus_labels = base_labels.clone();
-        prometheus_labels.extend(vec![("app".to_string(), "prometheus".to_string())].into_iter());
+        let base_labels: BTreeMap<String, String> = vec![
+            ("purpose".to_string(), "general".to_string()),
+            ("app".to_string(), "prometheus".to_string()),
+        ]
+        .into_iter()
+        .collect();
+        let deployment_name = "prometheus-deployment".to_string();
+        let service_name = "prometheus-service".to_string();
+        let config_map_name = "prometheus-config".to_string();
         Self {
             base_labels,
-            prometheus_labels,
+            deployment_name,
+            service_name,
+            config_map_name,
         }
+    }
+
+    pub async fn sync(&self, kube_client: KubeClient, running: bool) -> Result<()> {
+        let deployments: Api<Deployment> = Api::namespaced(kube_client.clone(), DEFAULT_NAMESPACE);
+        let config_maps: Api<ConfigMap> = Api::namespaced(kube_client.clone(), DEFAULT_NAMESPACE);
+        let services: Api<Service> = Api::namespaced(kube_client, DEFAULT_NAMESPACE);
+
+        if running {
+            let params = PatchParams::apply("battery_operator");
+            let dep_patch = Patch::Apply(self.build_prometheus_deployment(2)?);
+            deployments
+                .patch(&self.deployment_name, &params, &dep_patch)
+                .await?;
+
+            let cfg_patch = Patch::Apply(self.build_prometheus_configmap()?);
+            config_maps
+                .patch(&self.config_map_name, &params, &cfg_patch)
+                .await?;
+
+            let svc_patch = Patch::Apply(self.build_prometheus_service()?);
+            services
+                .patch(&self.service_name, &params, &svc_patch)
+                .await?;
+        }
+
+        Ok(())
     }
     pub fn build_prometheus_deployment(&self, replicas: i32) -> Result<Deployment> {
         Ok(serde_json::from_value(json!({
             "metadata":{
-                "name":"prometheus-deployment",
-                "labels": self.prometheus_labels.clone()
+                "name": self.deployment_name,
+                "labels": self.base_labels
             },
             "spec": {
                 "replicas": replicas,
                 "selector": {
-                    "matchLabels": self.prometheus_labels.clone(),
+                    "matchLabels": self.base_labels,
                 },
                 "template": {
                     "metadata":{
-                        "name":"prometheus-deployment",
-                        "labels": self.prometheus_labels.clone()
+                        "name":self.deployment_name,
+                        "labels": self.base_labels
                     },
                     "spec":{
                         "containers": [{
@@ -51,15 +93,14 @@ impl PrometheusInstaller {
                             "image": "prom/prometheus",
                             "volumeMounts": [{
                                 "name":"config-volume",
-                                "mountPath":"/etc/prometheus/prometheus.yml",
-                                "subPath":"prometheus.yml"
+                                "mountPath":"/etc/prometheus",
                             }],
-                            "ports": [{"containerPort": 9090}]
+                            "ports": [{"containerPort": 9090, "name": "default-port"}]
                         }],
                         "volumes": [{
                             "name":"config-volume",
                             "configMap": {
-                                "name": "prometheus-cm",
+                                "name": self.config_map_name,
                                 "optional": true,
                             }
                         }]
@@ -69,20 +110,34 @@ impl PrometheusInstaller {
         }))?)
     }
 
+    pub fn build_prometheus_configmap(&self) -> Result<ConfigMap> {
+        let yml_conents = "rule_files:
+        - '/etc/prometheus-rules/*.rules'";
+        Ok(serde_json::from_value(json!({
+            "metadata": {
+                "name": self.config_map_name,
+                "labels": self.base_labels.clone()
+            },
+            "data": {
+                "prometheus.yml":yml_conents,
+            }
+        }))?)
+    }
+
     pub fn build_prometheus_service(&self) -> Result<Service> {
         Ok(serde_json::from_value(json!({
             "metadata":{
-                "name":"prometheus",
-                "labels": self.prometheus_labels.clone()
+                "name":self.service_name,
+                "labels": self.base_labels,
             },
             "spec": {
-                "selector": self.prometheus_labels.clone()
+                "selector": self.base_labels,
+                "ports":[{
+                    "port": 9090,
+                    "targetPort": 9090,
+                    "protocol": "TCP"
+                }],
             },
-            "ports":[{
-                "name": 9090,
-                "targetPort": 9090,
-                "protocol": "TCP"
-            }]
         }))?)
     }
 }
