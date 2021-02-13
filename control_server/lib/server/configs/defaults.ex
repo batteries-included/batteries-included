cd1e69c194313eebeba222cb765536cef3c5bf70
Elliott Clark <elliott.neil.clark@gmail.com>
2021-02-13 01:28:36 +0000

    Use Ecto.Multi in defaults

--- control_server/lib/server/configs/defaults.ex
+++ control_server/lib/server/configs/defaults.ex
@@ -3,13 +3,24 @@ defmodule Server.Configs.Defaults do
   Module for creating the default configs for a new KubeCluster
   """
   import Ecto.Query, warn: false
+  alias Ecto.Multi
 
   alias Server.Configs.Adoption
+  alias Server.Configs.Prometheus
   alias Server.Configs.RunningSet
+  alias Server.Repo
 
   def create_all(kube_cluster_id) do
-    {:ok, adoption_config} = Adoption.create_for_cluster(kube_cluster_id)
-    {:ok, running_set} = RunningSet.create_for_cluster(kube_cluster_id)
-    {:ok, running_set: running_set, adoption_config: adoption_config}
+    Multi.new()
+    |> Multi.run(:adoption_config, fn _repo, _ ->
+      Adoption.create_for_cluster(kube_cluster_id)
+    end)
+    |> Multi.run(:running_set_config, fn _repo, _ ->
+      RunningSet.create_for_cluster(kube_cluster_id)
+    end)
+    |> Multi.run(:prometheus_config, fn _repo, _ ->
+      Prometheus.create_for_cluster(kube_cluster_id)
+    end)
+    |> Repo.transaction()
   end
 end
