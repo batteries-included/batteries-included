4c2a32592e74e7ae94644963b218adf7b620b9d9
Elliott Clark <elliott.neil.clark@gmail.com>
2021-03-06 23:19:52 +0000

    Bye operator

--- operator/controller_core/src/actors/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod cluster_controller;
-pub mod config_fetcher;
-pub mod core_messages;
-pub mod deps_ready;
-pub mod crd;
-pub mod namespace;
-pub mod root;
--- operator/controller_core/src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub mod actors;
-pub mod cs_client;
-pub mod metrics;
-pub mod prometheus;
-pub mod state;
