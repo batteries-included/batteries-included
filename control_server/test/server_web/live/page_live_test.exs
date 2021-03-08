c6238fd3bada166e87154ca365f6bdba8066521b
Elliott Clark <elliott.neil.clark@gmail.com>
2021-03-08 06:05:30 +0000

    Working toggle on running set.

--- control_server/test/server_web/live/page_live_test.exs
+++ control_server/test/server_web/live/page_live_test.exs
@@ -1,9 +1,11 @@
 defmodule ServerWeb.PageLiveTest do
   use ServerWeb.ConnCase
+  alias Server.Configs.Defaults
 
   import Phoenix.LiveViewTest
 
   test "disconnected and connected render", %{conn: conn} do
+    Defaults.create_all()
     {:ok, page_live, disconnected_html} = live(conn, "/")
     assert disconnected_html =~ "Hello Batteries"
     assert render(page_live) =~ "Hello Batteries"
