fcc019175d8a51416900054432e5f605ae2469e4
Elliott Clark <elliott.neil.clark@gmail.com>
2021-03-27 23:16:34 +0000

    Don't add unused prometheus default config

--- control_server/test/server/computed_configs_test.exs
+++ control_server/test/server/computed_configs_test.exs
@@ -1,10 +1,3 @@
 defmodule Server.Configs.ComputedConfigsTest do
   use Server.DataCase
-
-  alias Server.ComputedConfigs
-
-  test "get" do
-    {:ok, config} = ComputedConfigs.get("/prometheus/main")
-    assert config.path == "/prometheus/main"
-  end
 end
--- control_server/test/server/configs/prometheus_test.exs
+++ control_server/test/server/configs/prometheus_test.exs
@@ -1,10 +1,3 @@
 defmodule Server.Configs.PrometheusTest do
   use Server.DataCase
-
-  alias Server.Configs.Prometheus
-
-  test "base_config" do
-    raw_config = Prometheus.base_config!()
-    assert raw_config.path == "/prometheus/base"
-  end
 end
