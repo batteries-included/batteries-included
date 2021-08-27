5cdf4b2cb6c4794909b6800ea70b219fe40eeb98
Elliott <elliott.neil.clark@gmail.com>
2021-08-27 12:08:43 -0700

    Move monitoring's ServiceMonitors into their own module. (#442)

--- platform_umbrella/apps/kube_resources/lib/kube_resources/monitoring/service_monitors.ex
+++ platform_umbrella/apps/kube_resources/lib/kube_resources/monitoring/service_monitors.ex
@@ -1,12 +1,7 @@
 defmodule KubeResources.ServiceMonitors do
   alias ControlServer.Services
   alias ControlServer.Services.BaseService
-  alias KubeResources.Grafana
-  alias KubeResources.KubeState
-  alias KubeResources.MonitoringSettings
-  alias KubeResources.NodeExporter
-  alias KubeResources.Prometheus
-  alias KubeResources.PrometheusOperator
+  alias KubeResources.MonitoringServiceMonitors
 
   def monitors do
     Enum.flat_map(Services.list_base_services(), &monitors/1)
@@ -15,13 +10,7 @@ defmodule KubeResources.ServiceMonitors do
   def monitors(%BaseService{service_type: service_type, config: config}),
     do: monitors(service_type, config)
 
-  def monitors(:monitoring, config) do
-    PrometheusOperator.monitors(config) ++
-      Prometheus.monitors(config) ++
-      Grafana.monitors(config) ++
-      NodeExporter.monitors(config) ++
-      KubeState.monitors(config)
-  end
+  def monitors(:monitoring, config), do: MonitoringServiceMonitors.monitors(config)
 
   def monitors(_, _), do: []
 end
