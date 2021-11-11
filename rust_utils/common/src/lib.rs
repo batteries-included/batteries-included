4af9cfa93184fe3b14346dd08b31a7526adb5321
Elliott <elliott.neil.clark@gmail.com>
2021-11-11 13:13:21 -0800

    Fix newer tracing subscriber logging with a new rust version (#610)

--- rust_utils/common/src/lib.rs
+++ rust_utils/common/src/lib.rs
@@ -1,5 +1,26 @@
 #![deny(clippy::all)]
 #![deny(clippy::nursery)]
+#![warn(
+    missing_debug_implementations,
+    rust_2018_idioms,
+    unreachable_pub,
+    bad_style,
+    const_err,
+    dead_code,
+    improper_ctypes,
+    non_shorthand_field_patterns,
+    no_mangle_generic_items,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    private_in_public,
+    unconditional_recursion,
+    unused,
+    unused_allocation,
+    unused_comparisons,
+    unused_parens,
+    while_true
+)]
 
 pub mod cluster_spec;
 pub mod error;
