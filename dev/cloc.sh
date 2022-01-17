aa9262f3665d85c8fe73d8d725259e1b4d391480
Elliott <elliott.neil.clark@gmail.com>
2022-01-17 12:42:49 -0800

    Script to get the current lines of code counting. sigh (#762)

---
+++ dev/cloc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -exuo pipefail
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+cloc --ignore-whitespace \
+    --exclude-dir=node_modules,deps,_build,.venv,_next,.next,target,assets,package-lock.json \
+    "${DIR}/../"
