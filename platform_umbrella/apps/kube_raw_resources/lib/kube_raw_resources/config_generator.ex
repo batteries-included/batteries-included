61df1c61962fe703c9aac92166ca774d459c18ed
Elliott Clark <elliott.neil.clark@gmail.com>
2022-02-21 15:16:13 -0600

    Make sure that control server can be bootstrapped.

--- platform_umbrella/apps/kube_raw_resources/lib/kube_raw_resources/config_generator.ex
+++ platform_umbrella/apps/kube_raw_resources/lib/kube_raw_resources/config_generator.ex
@@ -1,8 +1,9 @@
 defmodule KubeRawResources.ConfigGenerator do
   alias KubeRawResources.Battery
   alias KubeRawResources.Database
+  alias KubeRawResources.ControlServerResources
 
   def materialize(%{} = config, :database), do: Database.materialize(config)
   def materialize(%{} = config, :battery), do: Battery.materialize(config)
-  def materialize(%{}, _), do: Map.new()
+  def materialize(%{} = config, :control_server), do: ControlServerResources.materialize(config)
 end
