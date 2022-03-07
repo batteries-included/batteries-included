fab00e74b533c5d7faf03615b086bd168975c714
Elliott Clark <elliott.neil.clark@gmail.com>
2022-03-07 15:07:37 -0600

    Cleanup KubeState

--- platform_umbrella/apps/kube_state/lib/kube_state.ex
+++ platform_umbrella/apps/kube_state/lib/kube_state.ex
@@ -2,17 +2,4 @@ defmodule KubeState do
   @moduledoc """
   Documentation for `KubeState`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> KubeState.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
