c3a35f23c9ee85b62e0cf17d7890c1b63fbb4a08
Elliott Clark <elliott.neil.clark@gmail.com>
2022-03-18 16:18:38 -0500

    cloc shouldn't count cover directories

--- dev/cloc.sh
+++ dev/cloc.sh
@@ -4,5 +4,5 @@ set -exuo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cloc --ignore-whitespace \
-    --exclude-dir=node_modules,deps,_build,.venv,_next,.next,target,assets,package-lock.json \
+    --exclude-dir=node_modules,deps,_build,.venv,_next,.next,target,assets,package-lock.json,cover \
     "${DIR}/../"
