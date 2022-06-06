360658361dbd3049b92e7d8e8b834f142bb21d77
Patrick White <patrick@batteriesincl.com>
2022-06-06 17:15:08 +0000

    use clap Parser for purdy command line

--- rust_utils/cli/src/main.rs
+++ rust_utils/cli/src/main.rs
@@ -3,34 +3,32 @@
 #![deny(clippy::nursery)]
 #![allow(clippy::module_name_repetitions)]
 
-use clap::{App, AppSettings};
+//! CLI for Batteries Included
+
+use clap::Parser;
 use common::error::Result;
 use common::logging::try_init_logging;
-use tracing::debug;
 
 mod bootstrap;
 mod print_yaml;
 
+/// Entry point to all things command-line
+#[derive(clap::Parser)]
+#[clap(name="bi-cli", author)]
+pub enum Cli {
+    /// Bootstrap a new cluster
+    Bootstrap,
+    /// Print the shared yaml for bootstrap and control servers
+    PrintYaml,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     try_init_logging()?;
+    let app = Cli::parse();
 
-    let matches = App::new("batteries-included")
-        .version("1.0")
-        .author("Elliott Clark <elliott@batteriesincl.com>")
-        .about("Control the world/cluster")
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(App::new("bootstrap").about("Bootstrap a new cluster"))
-        .subcommand(
-            App::new("print_yaml").about("Print the shared yaml for bootstrap and control server"),
-        )
-        .get_matches();
-
-    match matches.subcommand() {
-        Some(("bootstrap", _)) => bootstrap::run().await?,
-        Some(("print_yaml", _)) => print_yaml::run()?,
-        _ => debug!("Unknown command"),
+    match app {
+        Cli::Bootstrap => bootstrap::run().await,
+        Cli::PrintYaml => print_yaml::run(),
     }
-
-    Ok(())
 }
