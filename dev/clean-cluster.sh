66a3686733604ee44502961cf3b8624469463ac7
Elliott <elliott.neil.clark@gmail.com>
2022-10-10 18:38:15 -0500

    Add a script to remove everything from the cluster (#1198)

---
+++ dev/clean-cluster.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -exuo pipefail
+
+deleteAllTagged() {
+  kubectl get -lbattery/managed=true ${1} \
+    | awk '{print $1}' \
+    | xargs kubectl delete ${1} || true
+}
+
+deleteAllNamespaced() {
+    kubectl get ${1} -lbattery/managed=true --all-namespaces  \
+        | awk '{print $1} {print $2}' \
+        | grep -ve NAME \
+        | xargs -L2 bash -c "kubectl delete ${1} -n \$0 \$1" || true
+}
+
+deleteCrds() {
+  kubectl get crds \
+    | grep -ve cattle \
+    | awk '{print $1}' \
+    | xargs kubectl delete crds || true
+}
+
+deleteRBAC() {
+  deleteAllTagged clusterrole
+  deleteAllTagged clusterrolebinding
+  kubectl get clusterrole \
+    | grep -E '(loki|battery|promtail|istio|metallb)' \
+    | awk '{print $1}' \
+    | xargs kubectl delete clusterrole || true
+  kubectl get clusterrolebinding \
+    | grep -E '(loki|battery|promtail|istio|metallb)' \
+    | awk '{print $1}' \
+    | xargs kubectl delete clusterrolebinding || true
+}
+
+deleteJunk() {
+  kubectl delete configmap istio-ca-root-cert || true
+}
+
+deleteAllTagged namespace
+deleteCrds
+deleteRBAC
+deleteJunk
+
+deleteAllNamespaced service
+deleteAllNamespaced configmap 
+deleteAllNamespaced secret
+deleteAllNamespaced deployment
+deleteAllNamespaced deployment
