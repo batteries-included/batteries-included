de0874649ad4a08a0208296d7c310770c8c194a7
Elliott <elliott.neil.clark@gmail.com>
2022-10-11 17:21:51 -0500

    Fix build local when there's no docker up (#1201)

--- dev/build-local.sh
+++ dev/build-local.sh
@@ -4,24 +4,22 @@ set -exuo pipefail
 # Grab the location we'll use it for yaml locations soon
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 export DOCKER_BUILDKIT=1
-REGISTRY_PORT=$(docker port battery-registry | cut -d: -f2)
 TAG=$(git describe --always --dirty --broken)
 
 pushImage() {
+  REGISTRY_PORT=$(docker port battery-registry | cut -d: -f2)
   docker push "localhost:${REGISTRY_PORT}/${1}:${2}"
 }
 
 pushd ${DIR}/../platform_umbrella
 docker build \
   -t battery/control:${TAG} \
-  -t localhost:${REGISTRY_PORT}/battery/control:${TAG} \
   .
 pushImage "battery/control" "${TAG}"
 
 docker build --build-arg RELEASE=home_base \
   --build-arg BINARY=bin/home_base \
   -t battery/home:${TAG} \
-  -t localhost:${REGISTRY_PORT}/battery/home:${TAG} \
   .
 
 pushImage "battery/home" "${TAG}"
