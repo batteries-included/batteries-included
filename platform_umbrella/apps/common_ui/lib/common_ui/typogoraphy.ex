e9c4360d208e274fef01854d1a385cc581b848fd
Elliott <elliott.neil.clark@gmail.com>
2022-11-18 21:35:57 -0600

    Mix update ver all (#1299)
    * Update all the mix things
    
    * Update the vscode settings
    
    * Small tweaks on typography

--- platform_umbrella/apps/common_ui/lib/common_ui/typogoraphy.ex
+++ platform_umbrella/apps/common_ui/lib/common_ui/typogoraphy.ex
@@ -1,36 +1,38 @@
 defmodule CommonUI.Typogoraphy do
   use Phoenix.Component
   import Phoenix.Component, except: [link: 1]
+  import CommonUI.CSSHelpers
 
   attr :class, :any, default: ""
-
   attr :base_class, :string, default: "text-4xl leading-7 text-pink-500 sm:text-3xl sm:truncate"
-
   slot :inner_block, required: true
+  attr :rest, :global
 
   def h1(assigns) do
     ~H"""
-    <h1 class={[@class, @base_class]}><%= render_slot(@inner_block) %></h1>
+    <h1 class={build_class([@class, @base_class])} {@rest}><%= render_slot(@inner_block) %></h1>
     """
   end
 
   attr :class, :any, default: ""
   attr :base_class, :string, default: "text-2xl sm:text-3xl font-bold leading-10 text-primary-500"
   slot :inner_block, required: true
+  attr :rest, :global
 
   def h2(assigns) do
     ~H"""
-    <h2 class={[@class, @base_class]}><%= render_slot(@inner_block) %></h2>
+    <h2 class={build_class([@class, @base_class])} {@rest}><%= render_slot(@inner_block) %></h2>
     """
   end
 
   attr :class, :any, default: ""
   attr :base_class, :string, default: "text-xl sm:text-2xl font-bold leading-6"
   slot :inner_block, required: true
+  attr :rest, :global
 
   def h3(assigns) do
     ~H"""
-    <h3 class={[@class, @base_class]}><%= render_slot(@inner_block) %></h3>
+    <h3 class={build_class([@class, @base_class])} {@rest}><%= render_slot(@inner_block) %></h3>
     """
   end
 end
