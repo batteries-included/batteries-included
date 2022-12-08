1e7ba8da536ca93a536ecb06d6b30245dc2dc059
zohnoës <zoe.n.chalfant@gmail.com>
2022-12-08 11:03:55 -0800

    Add a script to nuke deps and rebuild stuff (#1354)
    * Add a script to nuke deps and rebuild stuff
    
    * Fixup
    
    Co-authored-by: Zoë Chalfant <zoe@batteriesincl.com>

---
+++ dev/deps-nuke.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -exuo pipefail
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+pushd "${DIR}/../platform_umbrella"
+RET=0
+rm -rf _build deps .elixir_ls && mix deps.get && mix compile --force
+popd
