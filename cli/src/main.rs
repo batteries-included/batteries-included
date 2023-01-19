e284b4982250789af88765f2cc158b4062416996
zohnoës <zoe.n.chalfant@gmail.com>
2023-01-19 12:23:55 -0800

    More cli scaffolding and tests (#1486)
    * CLI Barebones
    
    * De-globalize fwd postgress flag
    
    * Remove spurious help enum value
    
    * Add second test
    
    * Extract help into namespace level constant
    
    * More scaffolding and tests
    
    * Fixup
    
    * Fixup formatting
    
    * More cleanup
    
    Co-authored-by: Zoë Chalfant <zoe@chlfnt.com>
    Co-authored-by: Zoë Chalfant <zoe@batteriesincl.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,8 @@
 use clap::Parser;
+use log::{error, trace};
+
+static NOT_IMPL: &str = "Not yet implemented";
+static DEFAULT_POSTGRES_FWD_TGT: &str = "battery-base/pg-control";
 
 #[derive(clap::Parser)]
 struct Args {
@@ -10,27 +14,27 @@ struct Args {
 enum CliAction {
     Create {
         #[clap(long)]
-        forward_postgres: Option<String>,
-        #[clap(long)]
-        sync: Option<bool>
+        forward_postgres: Option<Option<String>>,
+        #[clap(long, default_value_t = false)]
+        sync: bool
     },
     Start {
         #[clap(long)]
-        forward_postgres: Option<String>,
+        forward_postgres: Option<Option<String>>,
         /// optional, positional customer id
         id: Option<String>,
 
-        #[clap(long)]
-        overwrite_resources: Option<bool>,
+        #[clap(long, default_value_t = false)]
+        overwrite_resources: bool,
     },
-
 }
 
 #[cfg(test)]
 mod tests {
-    use assert_cmd::prelude::*;
+    use assert_cmd::Command;
     use predicates::prelude::*;
-    use std::process::Command;
+    use predicates::str::contains;
+    use crate::DEFAULT_POSTGRES_FWD_TGT;
     static HELP_STR: &str = r#"Usage: cli <COMMAND>
 
 Commands:
@@ -41,11 +45,10 @@ Commands:
 Options:
   -h, --help  Print help
 "#;
+    static IMPL_ERR_STR: &str = "[ERROR cli] Not yet implemented";
 
     #[test]
     fn test_blank() {
-        use assert_cmd::Command;
-
         let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
         cmd.assert()
             .failure()
@@ -54,18 +57,176 @@ Options:
 
     #[test]
     fn test_help() {
-        use assert_cmd::Command;
-
         let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
         cmd.arg("help")
             .assert()
             .success()
             .stdout(predicate::eq(HELP_STR));
     }
+
+    #[test]
+    fn test_create_no_flags() {
+        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.arg("create")
+            .env("RUST_LOG", "trace")
+            .assert()
+            .failure()
+            .stderr(contains(format!("{}: create", IMPL_ERR_STR)))
+            .stdout(predicate::eq(""));
+    }
+
+    #[test]
+    fn test_create_foward_postgres_no_target_no_sync() {
+        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.arg("create")
+            .arg("--forward-postgres")
+            .env("RUST_LOG", "trace")
+            .assert()
+            .failure()
+            .stderr(contains(format!("{}: create", IMPL_ERR_STR)))
+            .stderr(contains(format!("forward-postgres: {}", DEFAULT_POSTGRES_FWD_TGT)))
+            .stdout(predicate::eq(""));
+    }
+
+    #[test]
+    fn test_create_foward_postgres_with_target_no_sync() {
+        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.arg("create")
+            .arg("--forward-postgres")
+            .arg("foo/bar")
+            .env("RUST_LOG", "trace")
+            .assert()
+            .failure()
+            .stderr(contains(IMPL_ERR_STR))
+            .stderr(contains("forward-postgres: foo/bar"))
+            .stdout(predicate::eq(""));
+    }
+
+    #[test]
+    fn test_create_foward_postgres_with_target_with_sync() {
+        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.arg("create")
+            .arg("--forward-postgres")
+            .arg("foo/bar")
+            .arg("--sync")
+            .env("RUST_LOG", "trace")
+            .assert()
+            .failure()
+            .stderr(contains(IMPL_ERR_STR))
+            .stderr(contains("forward-postgres: foo/bar"))
+            .stderr(contains("sync: true"))
+            .stdout(predicate::eq(""));
+    }
+
+    #[test]
+    fn test_start_no_flags() {
+        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.arg("start")
+            .env("RUST_LOG", "trace")
+            .assert()
+            .failure()
+            .stderr(contains(IMPL_ERR_STR))
+            .stderr(contains("id: demo"))
+            .stdout(predicate::eq(""));
+    }
+
+    #[test]
+    fn test_start_forward_postgres_no_target() {
+        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.arg("start")
+            .arg("--forward-postgres")
+            .env("RUST_LOG", "trace")
+            .assert()
+            .failure()
+            .stderr(contains(IMPL_ERR_STR))
+            .stderr(contains("id: demo"))
+            .stderr(contains(format!("forward-postgres: {}", DEFAULT_POSTGRES_FWD_TGT)))
+            .stdout(predicate::eq(""));
+    }
+
+    #[test]
+    fn test_start_forward_postgres_with_target() {
+        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.arg("start")
+            .arg("--forward-postgres")
+            .arg("baz/qux")
+            .env("RUST_LOG", "trace")
+            .assert()
+            .failure()
+            .stderr(contains(IMPL_ERR_STR))
+            .stderr(contains("id: demo"))
+            .stderr(contains("forward-postgres: baz/qux"))
+            .stdout(predicate::eq(""));
+    }
+
+    #[test]
+    fn test_start_forward_postgres_with_target_with_overwrite_resources() {
+        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+        cmd.arg("start")
+            .arg("--forward-postgres")
+            .arg("baz/qux")
+            .arg("--overwrite-resources")
+            .env("RUST_LOG", "trace")
+            .assert()
+            .failure()
+            .stderr(contains(format!("{}: start", IMPL_ERR_STR)))
+            .stderr(contains("id: demo"))
+            .stderr(contains("forward-postgres: baz/qux"))
+            .stderr(contains("overwrite-resources: true"))
+            .stdout(predicate::eq(""));
+    }
+}
+
+fn forward_postgres_handle(fwd_postgres_opt: Option<Option<String>>) -> String {
+    let fwd_postgres = match fwd_postgres_opt {
+        None => {
+            "".to_string()
+        },
+        Some(None) => {
+            DEFAULT_POSTGRES_FWD_TGT.to_string()
+        },
+        Some(tgt_opt) => {
+            tgt_opt.unwrap().trim().to_string()
+        },
+    };
+    if !fwd_postgres.is_empty() {
+        trace!("forward-postgres: {}", fwd_postgres);
+    }
+    fwd_postgres
 }
 
 fn main() {
+    env_logger::builder()
+    .format_timestamp(None)
+    .init();
     let args = Args::parse();
+    match args.cli_action {
+        CliAction::Create { forward_postgres, sync } => {
+            let _fwd_postgres_tgt = forward_postgres_handle(forward_postgres);
+            if sync {
+                trace!("sync: true");
+            }
+            error!("{}: create", NOT_IMPL);
+            std::process::exit(exitcode::UNAVAILABLE);
+        },
+        CliAction::Start { forward_postgres, id, overwrite_resources} => {
+            let id_tgt = match id {
+                None => {
+                    "demo".to_string()
+                },
+                Some(tgt) => {
+                    tgt
+                },
+            };
+            let _fwd_postgres_tgt = forward_postgres_handle(forward_postgres);
+            if overwrite_resources {
+                trace!("overwrite-resources: true")
+            }
+            trace!("id: {}", id_tgt);
+            error!("{}: start", NOT_IMPL);
+            std::process::exit(exitcode::UNAVAILABLE);
+        },
+    };
 }
 
 
