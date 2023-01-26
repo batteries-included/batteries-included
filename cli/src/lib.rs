8988f111e56ec7037465ad9d9a2e4244e75c3be8
zohnoës <zoe.n.chalfant@gmail.com>
2023-01-26 15:58:30 -0800

    Reorganize cli tool for better future testing options (#1505)
    * [WIP] k8s client in rust work
    
    * Fixup broken tests for now
    
    * Fixup broken tests for now again
    
    * More rusting
    
    * Now some reorganization of tests and we'll be good for commit
    
    * Wooo working properly with no hacky errors or lifetime management
    
    * Provide mocked client instead of default
    
    * Provide mocked client without calling into infer config
    
    * Formatting
    
    * Simplify logic
    
    Co-authored-by: Zoë Chalfant <zoe@chlfnt.com>

---
+++ cli/src/lib.rs
@@ -0,0 +1,157 @@
+use std::io::{BufRead, Write};
+
+#[derive(clap::Parser)]
+pub struct Args {
+    #[command(subcommand)]
+    cli_action: CliAction,
+}
+
+#[derive(clap::Subcommand)]
+enum CliAction {
+    Create {
+        #[clap(long)]
+        forward_postgres: Option<Option<String>>,
+        #[clap(long, default_value_t = false)]
+        sync: bool,
+    },
+    Start {
+        #[clap(long)]
+        forward_postgres: Option<Option<String>>,
+        /// optional, positional customer id
+        id: Option<String>,
+
+        #[clap(long, default_value_t = false)]
+        overwrite_resources: bool,
+    },
+}
+
+pub mod konstants {
+    pub static DEFAULT_POSTGRES_FWD_TGT: &str = "battery-base/pg-control";
+    pub static NOT_IMPL: &str = "Not yet implemented";
+}
+
+mod helpers {
+    use k8s_openapi::api::core::v1::Pod;
+    use kube_client::{api::ListParams, Api};
+
+    use crate::konstants;
+
+    pub(crate) fn forward_postgres_handle(fwd_postgres_opt: &Option<Option<String>>) -> String {
+        let fwd_postgres = match fwd_postgres_opt {
+            None => "".to_string(),
+            Some(None) => konstants::DEFAULT_POSTGRES_FWD_TGT.to_string(),
+            Some(Some(tgt_opt)) => tgt_opt.trim().to_string(),
+        };
+        fwd_postgres
+    }
+
+    pub(crate) async fn get_pg_control_primary(
+        client: kube_client::Client,
+        cluster: &str,
+        namespace: &str,
+    ) -> Result<String, Box<dyn std::error::Error>> {
+        let pods: Api<Pod> = Api::namespaced(client, namespace);
+        let lp =
+            ListParams::default().labels(&format!("spilo-role=master,cluster-name={}", cluster));
+        let results = pods.list(&lp).await?;
+        let pod = results.iter().next();
+        match pod.and_then(|x| Some(x.metadata.name.as_ref().unwrap().to_string())) {
+            Some(p) => return Ok(p),
+            None => return Err("Pod doesn't have `name` attribute".into()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use clap::Parser;
+    use hyper::Body;
+    use k8s_openapi::http::{Request, Response};
+
+    use crate::konstants;
+    use crate::program_main;
+    use crate::Args;
+
+    use tower_test::mock;
+
+    #[tokio::test]
+    async fn test_blank() {
+        let mut err = Vec::new();
+        let mut out = Vec::new();
+        let input = "".as_bytes();
+        let rc = program_main(
+            Args::parse_from(["cli", "create"]),
+            Box::new(|| {
+                let (mock_service, _) = mock::pair::<Request<Body>, Response<Body>>();
+                return kube_client::Client::new(mock_service, "default");
+            }),
+            &mut err,
+            &input,
+            &mut out,
+        )
+        .await;
+        assert_eq!(rc, exitcode::UNAVAILABLE);
+        assert_eq!(
+            String::from_utf8(err).unwrap(),
+            format!("{}: create\n", konstants::NOT_IMPL)
+        );
+        assert_eq!(String::from_utf8(out).unwrap(), "");
+    }
+}
+
+fn log(stderr: &mut dyn Write, msg: &str) {
+    match write!(stderr, "{}", msg) {
+        _ => return,
+    }
+}
+
+pub async fn program_main<'a>(
+    args: Args,
+    kube_client_factory: Box<dyn Fn() -> kube_client::Client>,
+    stderr: &'a mut dyn Write,
+    _stdin: &'a dyn BufRead,
+    _stdout: &'a mut dyn Write,
+) -> exitcode::ExitCode {
+    let client = kube_client_factory();
+    match args.cli_action {
+        CliAction::Create {
+            ref forward_postgres,
+            sync,
+        } => {
+            let ref _fwd_postgres_tgt: String = helpers::forward_postgres_handle(&forward_postgres);
+            if sync {
+                log(stderr, "sync: true\n");
+            }
+
+            log(stderr, &format!("{}: create\n", konstants::NOT_IMPL));
+            return exitcode::UNAVAILABLE;
+        }
+        CliAction::Start {
+            forward_postgres,
+            id,
+            overwrite_resources,
+        } => {
+            let id_tgt: String = match id {
+                None => "demo".to_string(),
+                Some(tgt) => tgt.to_string(),
+            };
+            if overwrite_resources {
+                log(stderr, "overwrite-resources: true\n");
+            }
+            log(stderr, &format!("id: {}\n", id_tgt));
+            log(stderr, &format!("{}: start\n", konstants::NOT_IMPL));
+            let fwd_postgres_tgt = helpers::forward_postgres_handle(&forward_postgres);
+            if !(fwd_postgres_tgt.is_empty()) {
+                let (namespace, cluster) = fwd_postgres_tgt.split_once("/").unwrap();
+                let _pod = match helpers::get_pg_control_primary(client, cluster, namespace).await {
+                    Ok(p) => log(stderr, &format!("{}\n", p)),
+                    Err(e) => {
+                        log(stderr, &format!("Error: {}\n", e));
+                        return exitcode::TEMPFAIL;
+                    }
+                };
+            }
+            return exitcode::UNAVAILABLE;
+        }
+    };
+}
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,40 +1,14 @@
 use clap::Parser;
-use log::{error, trace};
-
-static NOT_IMPL: &str = "Not yet implemented";
-static DEFAULT_POSTGRES_FWD_TGT: &str = "battery-base/pg-control";
-
-#[derive(clap::Parser)]
-struct Args {
-    #[command(subcommand)]
-    cli_action: CliAction,
-}
-
-#[derive(clap::Subcommand)]
-enum CliAction {
-    Create {
-        #[clap(long)]
-        forward_postgres: Option<Option<String>>,
-        #[clap(long, default_value_t = false)]
-        sync: bool
-    },
-    Start {
-        #[clap(long)]
-        forward_postgres: Option<Option<String>>,
-        /// optional, positional customer id
-        id: Option<String>,
-
-        #[clap(long, default_value_t = false)]
-        overwrite_resources: bool,
-    },
-}
+use cli::program_main;
+use cli::Args;
+use std::io;
+use std::io::BufReader;
+use std::io::BufWriter;
 
 #[cfg(test)]
 mod tests {
     use assert_cmd::Command;
     use predicates::prelude::*;
-    use predicates::str::contains;
-    use crate::DEFAULT_POSTGRES_FWD_TGT;
     static HELP_STR: &str = r#"Usage: cli <COMMAND>
 
 Commands:
@@ -45,15 +19,6 @@ Commands:
 Options:
   -h, --help  Print help
 "#;
-    static IMPL_ERR_STR: &str = "[ERROR cli] Not yet implemented";
-
-    #[test]
-    fn test_blank() {
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-        cmd.assert()
-            .failure()
-            .stderr(predicate::eq(HELP_STR));
-    }
 
     #[test]
     fn test_help() {
@@ -63,170 +28,22 @@ Options:
             .success()
             .stdout(predicate::eq(HELP_STR));
     }
-
-    #[test]
-    fn test_create_no_flags() {
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-        cmd.arg("create")
-            .env("RUST_LOG", "trace")
-            .assert()
-            .failure()
-            .stderr(contains(format!("{}: create", IMPL_ERR_STR)))
-            .stdout(predicate::eq(""));
-    }
-
-    #[test]
-    fn test_create_foward_postgres_no_target_no_sync() {
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-        cmd.arg("create")
-            .arg("--forward-postgres")
-            .env("RUST_LOG", "trace")
-            .assert()
-            .failure()
-            .stderr(contains(format!("{}: create", IMPL_ERR_STR)))
-            .stderr(contains(format!("forward-postgres: {}", DEFAULT_POSTGRES_FWD_TGT)))
-            .stdout(predicate::eq(""));
-    }
-
-    #[test]
-    fn test_create_foward_postgres_with_target_no_sync() {
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-        cmd.arg("create")
-            .arg("--forward-postgres")
-            .arg("foo/bar")
-            .env("RUST_LOG", "trace")
-            .assert()
-            .failure()
-            .stderr(contains(IMPL_ERR_STR))
-            .stderr(contains("forward-postgres: foo/bar"))
-            .stdout(predicate::eq(""));
-    }
-
-    #[test]
-    fn test_create_foward_postgres_with_target_with_sync() {
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-        cmd.arg("create")
-            .arg("--forward-postgres")
-            .arg("foo/bar")
-            .arg("--sync")
-            .env("RUST_LOG", "trace")
-            .assert()
-            .failure()
-            .stderr(contains(IMPL_ERR_STR))
-            .stderr(contains("forward-postgres: foo/bar"))
-            .stderr(contains("sync: true"))
-            .stdout(predicate::eq(""));
-    }
-
-    #[test]
-    fn test_start_no_flags() {
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-        cmd.arg("start")
-            .env("RUST_LOG", "trace")
-            .assert()
-            .failure()
-            .stderr(contains(IMPL_ERR_STR))
-            .stderr(contains("id: demo"))
-            .stdout(predicate::eq(""));
-    }
-
-    #[test]
-    fn test_start_forward_postgres_no_target() {
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-        cmd.arg("start")
-            .arg("--forward-postgres")
-            .env("RUST_LOG", "trace")
-            .assert()
-            .failure()
-            .stderr(contains(IMPL_ERR_STR))
-            .stderr(contains("id: demo"))
-            .stderr(contains(format!("forward-postgres: {}", DEFAULT_POSTGRES_FWD_TGT)))
-            .stdout(predicate::eq(""));
-    }
-
-    #[test]
-    fn test_start_forward_postgres_with_target() {
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-        cmd.arg("start")
-            .arg("--forward-postgres")
-            .arg("baz/qux")
-            .env("RUST_LOG", "trace")
-            .assert()
-            .failure()
-            .stderr(contains(IMPL_ERR_STR))
-            .stderr(contains("id: demo"))
-            .stderr(contains("forward-postgres: baz/qux"))
-            .stdout(predicate::eq(""));
-    }
-
-    #[test]
-    fn test_start_forward_postgres_with_target_with_overwrite_resources() {
-        let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-        cmd.arg("start")
-            .arg("--forward-postgres")
-            .arg("baz/qux")
-            .arg("--overwrite-resources")
-            .env("RUST_LOG", "trace")
-            .assert()
-            .failure()
-            .stderr(contains(format!("{}: start", IMPL_ERR_STR)))
-            .stderr(contains("id: demo"))
-            .stderr(contains("forward-postgres: baz/qux"))
-            .stderr(contains("overwrite-resources: true"))
-            .stdout(predicate::eq(""));
-    }
 }
 
-fn forward_postgres_handle(fwd_postgres_opt: Option<Option<String>>) -> String {
-    let fwd_postgres = match fwd_postgres_opt {
-        None => {
-            "".to_string()
-        },
-        Some(None) => {
-            DEFAULT_POSTGRES_FWD_TGT.to_string()
-        },
-        Some(tgt_opt) => {
-            tgt_opt.unwrap().trim().to_string()
-        },
-    };
-    if !fwd_postgres.is_empty() {
-        trace!("forward-postgres: {}", fwd_postgres);
-    }
-    fwd_postgres
+#[tokio::main]
+async fn main() {
+    let stdin = BufReader::new(io::stdin().lock());
+    let mut stderr = BufWriter::new(io::stderr());
+    let mut stdout = BufWriter::new(io::stdout());
+
+    let code = program_main(
+        Args::parse(),
+        Box::new(|| futures::executor::block_on(kube_client::Client::try_default()).unwrap()),
+        &mut stderr,
+        &stdin,
+        &mut stdout,
+    )
+    .await;
+
+    std::process::exit(code);
 }
-
-fn main() {
-    env_logger::builder()
-    .format_timestamp(None)
-    .init();
-    let args = Args::parse();
-    match args.cli_action {
-        CliAction::Create { forward_postgres, sync } => {
-            let _fwd_postgres_tgt = forward_postgres_handle(forward_postgres);
-            if sync {
-                trace!("sync: true");
-            }
-            error!("{}: create", NOT_IMPL);
-            std::process::exit(exitcode::UNAVAILABLE);
-        },
-        CliAction::Start { forward_postgres, id, overwrite_resources} => {
-            let id_tgt = match id {
-                None => {
-                    "demo".to_string()
-                },
-                Some(tgt) => {
-                    tgt
-                },
-            };
-            let _fwd_postgres_tgt = forward_postgres_handle(forward_postgres);
-            if overwrite_resources {
-                trace!("overwrite-resources: true")
-            }
-            trace!("id: {}", id_tgt);
-            error!("{}: start", NOT_IMPL);
-            std::process::exit(exitcode::UNAVAILABLE);
-        },
-    };
-}
-
-
