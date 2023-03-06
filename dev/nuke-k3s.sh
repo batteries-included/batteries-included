a273794252d5ca69aa674796ca47605cf734b278
Elliott <elliott@batteriesincl.com>
2023-03-06 12:49:49 -0600

    chore: add nuke script for k3s (#1603)

---
+++ dev/nuke-k3s.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+shopt -s nullglob
+
+systemctl stop containerd k3s || true
+
+find /sys/fs/cgroup/systemd/system.slice/containerd.service* \
+  /sys/fs/cgroup/systemd/kubepods* \
+  /sys/fs/cgroup/kubepods* \
+  -name cgroup.procs | xargs -r cat | xargs -r kill -9
+mount | awk '/\/var\/lib\/kubelet|\/run\/netns|\/run\/containerd/ {print $3}' | xargs -r umount
+rm -rf /var/lib/rancher/ /var/lib/containerd /etc/rancher /run/containerd/ /var/lib/cni/
+systemctl start k3s
