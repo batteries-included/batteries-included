d0aa6f3fe29b90199c21d23648bdb9e22859cc74
Elliott <elliott@batteriesincl.com>
2023-05-09 01:25:50 -0500

    fix: take in more properties for card (#1708)

--- platform_umbrella/apps/common_ui/lib/common_ui/card.ex
+++ platform_umbrella/apps/common_ui/lib/common_ui/card.ex
@@ -4,18 +4,23 @@ defmodule CommonUI.Card do
   import CommonUI.Typogoraphy
 
   attr :class, :string, default: nil
+  attr :rest, :global, include: ~w(id)
+
   slot :inner_block
   slot :title
 
   def card(assigns) do
     ~H"""
-    <div class={
-      build_class([
-        "p-4 bg-white rounded-lg border border-gray-200 shadow-lg",
-        "flex flex-col",
-        @class
-      ])
-    }>
+    <div
+      class={
+        build_class([
+          "p-4 bg-white rounded-lg border border-gray-200 shadow-lg",
+          "flex flex-col",
+          @class
+        ])
+      }
+      {@rest}
+    >
       <.h2 :if={@title != nil and @title != []}>
         <%= render_slot(@title) %>
       </.h2>
