336efd1be2e10a374c6514337e49a36a4207665a
Elliott <elliott@batteriesincl.com>
2023-05-22 16:23:38 -0500

    chore: regularize the dev script names (#1751)
    Summary:
    Regularize the start of dev script names.
    
    nuke: deletes things removing data
    gen: creates some code or config from existing data
    
    Test Plan:
    nix flake check

--- dev/blame.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-git ls-tree -zr --name-only HEAD |
-  xargs -0 -n1 git blame --line-porcelain HEAD |
-  grep -ae "^author " |
-  sort |
-  uniq -c |
-  sort -nr
--- dev/start-wg-client.sh
+++ dev/start-wg-client.sh
@@ -4,5 +4,5 @@ set -exuo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 wg-quick down wg0 || true
-${DIR}/wireguard-client-config.sh "${1}" | tee /etc/wireguard/wg0.conf
+${DIR}/gen-wireguard-client-config.sh "${1}" | tee /etc/wireguard/wg0.conf
 wg-quick up wg0
