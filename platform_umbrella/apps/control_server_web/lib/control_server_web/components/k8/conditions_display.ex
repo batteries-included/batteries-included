1a2ea20eb6640b99324fafedc9304c98902d3a4a
Elliott <elliott@batteriesincl.com>
2023-07-13 11:45:54 -0500

    ui: tweak conditions display (#1934)
    There are conditions with no message present even after the conditions are met. So we need
    to check for empty string or no message for every condition as a way of determining which
    is good
    
    test:
    
    Visual Checking.
    I'll attach a screen shot

--- platform_umbrella/apps/control_server_web/lib/control_server_web/components/k8/conditions_display.ex
+++ platform_umbrella/apps/control_server_web/lib/control_server_web/components/k8/conditions_display.ex
@@ -2,18 +2,35 @@ defmodule ControlServerWeb.ConditionsDisplay do
   use ControlServerWeb, :html
 
   attr :conditions, :list, required: true
+  attr :empty, :boolean, default: nil, required: false
 
-  def conditions_display(assigns = %{conditions: conditions}) when conditions == [] do
+  def conditions_display(%{empty: nil} = assigns) do
+    conditions = Map.get(assigns, :conditions, [])
+
+    assigns =
+      Map.put(
+        assigns,
+        :empty,
+        Enum.all?(conditions, fn c ->
+          m = c["message"] || nil
+          m == nil || m == ""
+        end)
+      )
+
+    conditions_display(assigns)
+  end
+
+  def conditions_display(%{empty: true} = assigns) do
     ~H"""
     <.h2 class="flex items-center">
-      Messages - <span class="text-base text-gray-500 pt-1 pl-3">No new messages!</span>
+      Conditions - <span class="text-base text-gray-500 pt-1 pl-3">no outstanding messages!</span>
     </.h2>
     """
   end
 
   def conditions_display(assigns) do
     ~H"""
-    <.h2>Messages</.h2>
+    <.h2>Conditions</.h2>
     <.table rows={Enum.sort_by(@conditions, &get_condition_time/1, :desc)}>
       <:col :let={condition} label="Type"><%= Map.get(condition, "type", "") %></:col>
       <:col :let={condition} label="Message"><%= Map.get(condition, "message", "") %></:col>
