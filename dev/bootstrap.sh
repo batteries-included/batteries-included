3edc24ae91aa929ead0f887458ee9b3d8e61f934
Elliott <elliott@batteriesincl.com>
2023-08-01 18:06:14 -0500

    fix: fully expand DIR to make sure chdir works with bootstrap (#2032)

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -xuo pipefail
 
-# Grab the location we'll use it for yaml locations soon
-DIR="${BASH_SOURCE%/*}"
+# Grab the location we'll use it for lots later on
+#
+# We fully expand it as well so that rust chdir works when calling into mix
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 trap 'trap - SIGTERM && kill -- -$$' SIGINT SIGTERM EXIT
 
