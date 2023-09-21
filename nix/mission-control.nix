92426d0baa26b2214eea04c30cd2ceeee074d05b
Jason Tarasovic <JTarasovic@users.noreply.github.com>
2023-09-21 10:39:16 -0500

    fix: revert superfluous mix env setting (#2319)
    Co-authored-by: Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>

--- nix/mission-control.nix
+++ nix/mission-control.nix
@@ -35,7 +35,6 @@
               [[ -z ''${TRACE:-""} ]] || set -x
               pushd platform_umbrella &> /dev/null
               trap 'popd &> /dev/null' EXIT
-              export MIX_ENV=test
               mix test --trace --stale
             '';
           };
@@ -47,7 +46,6 @@
               [[ -z ''${TRACE:-""} ]] || set -x
               pushd platform_umbrella &> /dev/null
               trap 'popd &> /dev/null' EXIT
-              export MIX_ENV=test
               mix test --trace --exclude slow --cover --export-coverage default --warnings-as-errors
               mix test.coverage
             '';
