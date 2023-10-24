12b347c361545d67cca27dc7b6ed6939e6f66254
Jason Tarasovic <JTarasovic@users.noreply.github.com>
2023-10-24 09:09:36 -0500

    chore: clean up flake input formatting (#2508)
    Co-authored-by: Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -3,15 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     # provides arch os pairs for flake-utils
     systems.url = "github:nix-systems/default";
+
     # provides additional utilities for working with flakes
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.systems.follows = "systems";
     };
+
     # utilities for finding the flake root (e.g. where is flake.nix)
     flake-root.url = "github:srid/flake-root";
+
     # provides rust toolchain
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -20,20 +24,19 @@
         flake-utils.follows = "flake-utils";
       };
     };
+
     # library for working w/ cargo
     crane = {
       url = "github:ipetkov/crane";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        rust-overlay.follows = "rust-overlay";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
     # vuln db for crates
     advisory-db = {
       url = "github:rustsec/advisory-db";
       flake = false;
     };
+
     # converts NPM deps to work w/ Nix
     npmlock2nix = {
       url = "github:nix-community/npmlock2nix";
@@ -43,19 +46,22 @@
     # helper for modularizing flakes
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
-      inputs = { nixpkgs-lib.follows = "nixpkgs"; };
+      inputs.nixpkgs-lib.follows = "nixpkgs";
     };
+
     # make nix emulate .gitignore via helpers
     gitignore = {
       url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     # treefmt for nix. formats everything but elixir
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
-      inputs = { nixpkgs.follows = "nixpkgs"; };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # provides custom CLI - e.g. bi commands
     mission-control = {
       url = "github:Platonic-Systems/mission-control";
     };
