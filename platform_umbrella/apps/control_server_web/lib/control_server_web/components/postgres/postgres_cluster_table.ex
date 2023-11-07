787dcf7f6f7e8f9261094f1153fa3f6fbc4408f7
Elliott <elliott@batteriesincl.com>
2023-11-07 14:45:50 -0600

    style: add action icons and clickable rows for pg and redis (#2580)
    Summary:
    On the data home page these should have icons that can be clicked. So
    this adds the show and edit icons as well as adding a row click handler
    for every row.
    
    Test Plan:
    Used the page.

--- platform_umbrella/apps/control_server_web/lib/control_server_web/components/postgres/postgres_cluster_table.ex
+++ platform_umbrella/apps/control_server_web/lib/control_server_web/components/postgres/postgres_cluster_table.ex
@@ -2,25 +2,44 @@ defmodule ControlServerWeb.PostgresClusterTable do
   @moduledoc false
   use ControlServerWeb, :html
 
+  alias CommonCore.Postgres.Cluster
+
   attr :id, :string, default: "postgres-cluster-table"
   attr :rows, :list, required: true
   attr :abbridged, :boolean, default: false, doc: "the abbridged property control display of the id column and formatting"
 
+  @spec postgres_clusters_table(map()) :: Phoenix.LiveView.Rendered.t()
   def postgres_clusters_table(assigns) do
     ~H"""
-    <.table id={@id} rows={@rows}>
+    <.table id={@id} rows={@rows} row_click={&JS.navigate(show_url(&1))}>
       <:col :let={pg} :if={!@abbridged} label="ID"><%= pg.id %></:col>
       <:col :let={pg} label="Name"><%= pg.name %></:col>
       <:col :let={pg} label="Type"><%= pg.type %></:col>
       <:col :let={pg} label="User Count"><%= length(pg.users) %></:col>
       <:action :let={pg}>
-        <.a navigate={show_url(pg)} variant="styled">
-          Show
-        </.a>
+        <.action_icon
+          to={show_url(pg)}
+          icon={:eye}
+          tooltip={"Show Postgres cluster " <> pg.name}
+          id={"show_postgres_" <> pg.id}
+        />
+      </:action>
+
+      <:action :let={pg}>
+        <.action_icon
+          to={edit_url(pg)}
+          icon={:pencil}
+          tooltip={"Edit cluster " <> pg.name}
+          id={"edit_postgres_" <> pg.id}
+        />
       </:action>
     </.table>
     """
   end
 
-  defp show_url(cluster), do: ~p"/postgres/#{cluster}/show"
+  @spec show_url(Cluster.t()) :: String.t()
+  defp show_url(%Cluster{} = cluster), do: ~p"/postgres/#{cluster}/show"
+
+  @spec edit_url(Cluster.t()) :: String.t()
+  defp edit_url(%Cluster{} = cluster), do: ~p"/postgres/#{cluster}/edit"
 end
--- platform_umbrella/apps/control_server_web/lib/control_server_web/components/redis/redis_table.ex
+++ platform_umbrella/apps/control_server_web/lib/control_server_web/components/redis/redis_table.ex
@@ -2,22 +2,43 @@ defmodule ControlServerWeb.RedisTable do
   @moduledoc false
   use ControlServerWeb, :html
 
-  def show_url(cluster), do: ~p"/redis/#{cluster}/show"
+  alias CommonCore.Redis.FailoverCluster
 
   attr :rows, :list, default: []
   attr :abbridged, :boolean, default: false, doc: "the abbridged property control display of the id column and formatting"
 
+  @spec redis_table(map()) :: Phoenix.LiveView.Rendered.t()
   def redis_table(assigns) do
     ~H"""
-    <.table id="redis-display-table" rows={@rows}>
+    <.table id="redis-display-table" rows={@rows} row_click={&JS.navigate(show_url(&1))}>
       <:col :let={redis} :if={!@abbridged} label="ID"><%= redis.id %></:col>
       <:col :let={redis} label="Name"><%= redis.name %></:col>
       <:col :let={redis} label="Instances"><%= redis.num_redis_instances %></:col>
       <:col :let={redis} label="Sentinel Instances"><%= redis.num_sentinel_instances %></:col>
       <:action :let={redis}>
-        <.a navigate={show_url(redis)} variant="styled">Show</.a>
+        <.action_icon
+          to={show_url(redis)}
+          icon={:eye}
+          tooltip={"Show Redis failover cluster " <> redis.name}
+          id={"show_redis_" <> redis.id}
+        />
+      </:action>
+
+      <:action :let={redis}>
+        <.action_icon
+          to={edit_url(redis)}
+          icon={:pencil}
+          tooltip={"Edit cluster " <> redis.name}
+          id={"edit_redis_" <> redis.id}
+        />
       </:action>
     </.table>
     """
   end
+
+  @spec show_url(FailoverCluster.t()) :: String.t()
+  def show_url(%FailoverCluster{} = cluster), do: ~p"/redis/#{cluster}/show"
+
+  @spec edit_url(FailoverCluster.t()) :: String.t()
+  def edit_url(%FailoverCluster{} = cluster), do: ~p"/redis/#{cluster}/show"
 end
