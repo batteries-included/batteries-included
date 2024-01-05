495a90cfe78e431772682e22587b699b5c4aabdf
Jason Tarasovic <JTarasovic@users.noreply.github.com>
2024-01-04 21:06:51 -0600

    feat(ops/aws): make clusters private (#2881)
    Co-authored-by: Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>

--- ops/aws/terraform/eks.tf
+++ ops/aws/terraform/eks.tf
@@ -9,9 +9,20 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name                   = local.eks.cluster_name
-  cluster_version                = local.eks.cluster_version
-  cluster_endpoint_public_access = true
+  cluster_name                    = local.eks.cluster_name
+  cluster_version                 = local.eks.cluster_version
+  cluster_endpoint_public_access  = false
+  cluster_endpoint_private_access = true
+  cluster_security_group_additional_rules = {
+    gateway_source_security_group_id = {
+      description              = "Ingress from gateway SG to k8s control plane"
+      protocol                 = "tcp"
+      from_port                = 443
+      to_port                  = 443
+      type                     = "ingress"
+      source_security_group_id = aws_security_group.wireguard.id
+    }
+  }
 
   cluster_addons = {
     aws-ebs-csi-driver = {
@@ -37,7 +48,7 @@ module "eks" {
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.public_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
 
   kms_key_deletion_window_in_days = 7
 
