b8cbdf62990b497f0c95ed3ee35f98528ec9fa73
Elliott <elliott@batteriesincl.com>
2024-02-15 13:23:16 -0600

    chore: update elixir deps (#3069)
    Summary:
    The js side went in today this is the update to elxir side of th live
    view.
    
    Test Plan:
    `nix flake check`
    `nix build ".#control-server"`

--- nix/platform.nix
+++ nix/platform.nix
@@ -37,7 +37,7 @@
         inherit src version LANG;
         mixEnv = "test";
         #sha256 = lib.fakeSha256;
-        sha256 = "sha256-BbRSvFLHQtTwKIsbERBioRjKCSkjyVfZZ7HTdGoLW6c=";
+        sha256 = "sha256-t0jng+97YKBh7F3v1KJ0wVVmKcv1J5hoW2T80cBP8BI=";
       };
 
       # mix fixed output derivation dependencies
@@ -50,7 +50,7 @@
         pname = "mix-deps-platform";
         inherit src version LANG;
         #sha256 = lib.fakeSha256;
-        sha256 = "sha256-tTn6qKh291RV9Zxf5NT70Cjy/dpz6htcuYqS68eYOIk=";
+        sha256 = "sha256-jQ99IC7HrLh28xvOBIFFQKelhkShcFWBim7R/D2Djz0=";
       };
 
       control-server = pkgs.callPackage ./platform_release.nix {
