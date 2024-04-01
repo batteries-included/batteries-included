42b6079967ea0a9c8df38215baf7f4998b9a8f9a
Elliott <elliott@batteriesincl.com>
2024-04-01 16:34:26 -0500

    fix: default to no wireguard-config (#3281)
    Summary:
    No local clusters use wireguard so default to this off.
    
    Test Plan:
    cleaned a kube cluster with the new default.

--- bi/cmd/cmdutil/wireguard_config.go
+++ bi/cmd/cmdutil/wireguard_config.go
@@ -1,11 +1,9 @@
 package cmdutil
 
 import (
-	"github.com/adrg/xdg"
 	"github.com/spf13/cobra"
 )
 
 func AddWireGuardConfigFlag(cmd *cobra.Command) {
-	wireGuardConfigPath, _ := xdg.ConfigFile("bi/wireguard.yaml")
-	cmd.PersistentFlags().String("wireguard-config", wireGuardConfigPath, "The wireguard config to use")
+	cmd.PersistentFlags().String("wireguard-config", "", "The wireguard config to use")
 }
