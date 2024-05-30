6d48af488860657ba9860f3bbbda3729a7d6190b
Jason Maurer <jsonmaur@fastmail.com>
2024-05-29 18:13:00 -0600

    Add ticks and boundaries to range input

--- platform_umbrella/apps/common_ui/storybook/components/input/range.story.exs
+++ platform_umbrella/apps/common_ui/storybook/components/input/range.story.exs
@@ -3,7 +3,7 @@ defmodule Storybook.Components.Input.Range do
   use PhoenixStorybook.Story, :component
 
   def function, do: &CommonUI.Components.Input.input/1
-  def container, do: {:div, class: "w-full"}
+  def container, do: {:div, class: "w-full p-4"}
 
   def variations do
     [
@@ -12,7 +12,6 @@ defmodule Storybook.Components.Input.Range do
         attributes: %{
           type: "range",
           name: "foobar",
-          label: "Label",
           value: 2,
           step: 1,
           max: 5,
@@ -24,12 +23,63 @@ defmodule Storybook.Components.Input.Range do
         attributes: %{
           type: "range",
           name: "foobar",
-          label: "Label",
           show_value: false,
           value: 4,
           step: 1,
           max: 5,
           min: 1
+        }
+      },
+      %Variation{
+        id: :with_error,
+        attributes: %{
+          type: "range",
+          name: "foobar",
+          show_value: false,
+          value: 4,
+          step: 1,
+          max: 5,
+          min: 1,
+          errors: ["Oh no"],
+          force_feedback: true
+        }
+      },
+      %Variation{
+        id: :with_ticks,
+        attributes: %{
+          type: "range",
+          name: "foobar",
+          show_value: false,
+          value: 50,
+          step: 1,
+          max: 100,
+          ticks: [
+            {"0%", 0},
+            {"15%", 0.15},
+            {"50%", 0.5},
+            {"75%", 0.75},
+            {"100%", 1}
+          ]
+        }
+      },
+      %Variation{
+        id: :with_boundaries,
+        attributes: %{
+          type: "range",
+          name: "foobar",
+          show_value: false,
+          value: 50,
+          step: 1,
+          max: 100,
+          lower_boundary: 15,
+          upper_boundary: 75,
+          ticks: [
+            {"0%", 0},
+            {"15%", 0.15},
+            {"50%", 0.5},
+            {"75%", 0.75},
+            {"100%", 1}
+          ]
         }
       }
     ]
