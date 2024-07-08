76a8a93ab11c046f54b3aa3e4a1f3d82f2c97f41
Elliott Clark <elliott@batteriesincl.com>
2024-07-08 15:37:17 -0500

    build: add dependabot for bi and pastebin-go
    Summary:
    Now that we're not using gomod2nix we can use dependabot to update the
    deps
    
    Test Plan:
    dependabot comes around

--- platform_umbrella/apps/common_core/lib/common_core/version.ex
+++ platform_umbrella/apps/common_core/lib/common_core/version.ex
@@ -1,12 +1,22 @@
 defmodule CommonCore.Version do
-  @moduledoc false
+  @moduledoc """
+  Module for getting the version and hash of the current build.
+
+  This uses git to get the hash and the version from the mix.exs file.
+  """
+
+  @args ["describe", "--match=\"badtagthatnevermatches\"", "--always", "--dirty"]
 
   @hash "git"
-        |> System.cmd(["describe", "--match=\"badtagthatnevermatches\"", "--always", "--dirty"])
+        |> System.cmd(@args)
         |> elem(0)
         |> String.trim()
+
   @version Mix.Project.config()[:version]
 
+  @spec version() :: String.t()
   def version, do: @version
+
+  @spec hash() :: String.t()
   def hash, do: @hash
 end
