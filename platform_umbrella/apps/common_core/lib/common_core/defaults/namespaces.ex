89d925eae51a82927d9bdd6cb59e8d626dc329e6
Jason Maurer <jsonmaur@fastmail.com>
2024-07-15 13:28:31 -0600

    No phoenix in common_core

--- platform_umbrella/apps/common_core/lib/common_core/defaults/namespaces.ex
+++ platform_umbrella/apps/common_core/lib/common_core/defaults/namespaces.ex
@@ -1,5 +1,6 @@
 defmodule CommonCore.Defaults.Namespaces do
   @moduledoc false
+
   def core, do: "battery-core"
   def base, do: "battery-base"
   def data, do: "battery-data"
@@ -8,11 +9,11 @@ defmodule CommonCore.Defaults.Namespaces do
   def knative, do: "battery-knative"
   def backend, do: "battery-backend"
 
+  def humanize("battery-core"), do: "Core"
+  def humanize("battery-base"), do: "Base"
+  def humanize("battery-data"), do: "Data"
   def humanize("battery-ai"), do: "AI"
-
-  def humanize(ns) do
-    ns
-    |> String.trim_leading("battery-")
-    |> Phoenix.Naming.humanize()
-  end
+  def humanize("battery-istio"), do: "Istio"
+  def humanize("battery-knative"), do: "Knative"
+  def humanize("battery-backend"), do: "Backend"
 end
