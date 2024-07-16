6843a4ee7b5a4f402fdeaa05fdf9a79ef68e9f49
Elliott Clark <elliott@batteriesincl.com>
2024-07-16 11:00:27 -0500

    pref: speed up startup by unifying migrations
    Summary:
    When starting control server we need to check each migration and apply
    it linearly. While this isn't too much of an issue now, it adds up and
    makes startup time degrade.
    
    This unifies all of the migrations in control server into one
    
    Test Plan:
    Tests still pass

--- platform_umbrella/apps/control_server/priv/repo/migrations/20240630172240_add_traditional_services_project.exs
+++
@@ -1,9 +0,0 @@
-defmodule ControlServer.Repo.Migrations.AddTraditionalServicesProject do
-  use Ecto.Migration
-
-  def change do
-    alter table(:traditional_services) do
-      add :project_id, references(:projects)
-    end
-  end
-end
--- platform_umbrella/apps/control_server/priv/repo/migrations/20240709071356_remove_project_type.exs
+++
@@ -1,9 +0,0 @@
-defmodule ControlServer.Repo.Migrations.RemoveProjectType do
-  use Ecto.Migration
-
-  def change do
-    alter table(:projects) do
-      remove :type
-    end
-  end
-end
