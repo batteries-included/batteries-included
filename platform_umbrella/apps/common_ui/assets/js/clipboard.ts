1ea8aeb0e975e27fd55f671686b07926e8cae8af
Jason Maurer <jsonmaur@fastmail.com>
2024-07-29 16:01:21 -0600

    Fix JS formatting

--- platform_umbrella/apps/common_ui/assets/js/clipboard.ts
+++ platform_umbrella/apps/common_ui/assets/js/clipboard.ts
@@ -15,25 +15,27 @@ export const ClipboardHook = {
     this.clipboardIcon = document.querySelector(`#${this.el.id}-icon`);
     this.checkmarkIcon = document.querySelector(`#${this.el.id}-check`);
 
-    this.el.addEventListener("click", (event) => this.copyToClipboard(event));
+    this.el.addEventListener('click', (event) => this.copyToClipboard(event));
   },
   destroyed() {
-    this.el.removeEventListener("click", (event) => this.copyToClipboard(event));
+    this.el.removeEventListener('click', (event) =>
+      this.copyToClipboard(event)
+    );
   },
   async copyToClipboard(event) {
     event.preventDefault();
 
     await navigator.clipboard.writeText(this.contents);
-    
+
     this.showCheck();
     setTimeout(() => this.hideCheck(), 2000);
   },
   showCheck() {
-    this.clipboardIcon.classList.add("hidden");
-    this.checkmarkIcon.classList.remove("hidden");
+    this.clipboardIcon.classList.add('hidden');
+    this.checkmarkIcon.classList.remove('hidden');
   },
   hideCheck() {
-    this.clipboardIcon.classList.remove("hidden");
-    this.checkmarkIcon.classList.add("hidden");
-  }
+    this.clipboardIcon.classList.remove('hidden');
+    this.checkmarkIcon.classList.add('hidden');
+  },
 } as ClipboardHookInterface;
