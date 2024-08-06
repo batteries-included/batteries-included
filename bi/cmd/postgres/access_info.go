7a5eb50b5dda46da540402aac54298f4f2eca6df
Elliott Clark <elliott@batteriesincl.com>
2024-08-06 13:09:12 -0500

    feat: json for postgres access printing
    Summary:
    - clean up the code
    - Add json for the whole secret

--- bi/cmd/postgres/access_info.go
+++ bi/cmd/postgres/access_info.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"bi/pkg/installs"
 	"bi/pkg/log"
+	"encoding/json"
 	"fmt"
 	"net/url"
 
@@ -39,20 +40,36 @@ var accessInfoCmd = &cobra.Command{
 			return fmt.Errorf("failed to get postgres access info: %v", err)
 		}
 
-		dsn := postgresAccessSpec.DSN
-		u, err := url.Parse(dsn)
-		if err != nil {
-			return fmt.Errorf("failed to parse DSN: %v", err)
-		}
-
+		// If the user is trying to connect to a database they likely
+		// are portforwarding to a database that's in a kube cluster
+		//
+		// Change the hostname to localhost and set sslmode to allow
+		// so that the user can connect to the database
 		if cmd.Flag("localhost").Value.String() == "true" {
+			dsn := postgresAccessSpec.DSN
+			u, err := url.Parse(dsn)
+			if err != nil {
+				return fmt.Errorf("failed to parse DSN: %v", err)
+			}
 			u.Host = "localhost"
 			q := u.Query()
 			q.Set("sslmode", "allow")
 			u.RawQuery = q.Encode()
+
+			// Change the access spec to reflect the changes
+			postgresAccessSpec.Hostname = "localhost"
+			postgresAccessSpec.DSN = u.String()
 		}
 
-		fmt.Println(u)
+		if cmd.Flag("json").Value.String() == "true" {
+			value, err := json.Marshal(postgresAccessSpec)
+			if err != nil {
+				return fmt.Errorf("failed to marshal postgres access spec: %v", err)
+			}
+			fmt.Println(string(value))
+		} else {
+			fmt.Println(postgresAccessSpec.DSN)
+		}
 
 		return nil
 	},
@@ -62,4 +79,5 @@ func init() {
 	postgresCmd.AddCommand(accessInfoCmd)
 	accessInfoCmd.PersistentFlags().StringP("namespace", "n", "battery-core", "The namespace to use")
 	accessInfoCmd.PersistentFlags().BoolP("localhost", "l", false, "Use localhost instead of the hostname name")
+	accessInfoCmd.PersistentFlags().BoolP("json", "j", false, "Output in JSON format")
 }
