a2126beb460977903fe1c81b2d7bde2cb2dfccc2
Jason Tarasovic <JTarasovic@users.noreply.github.com>
2024-08-28 15:20:58 -0500

    chore(core): validate image struct (#734)
    It's probably valuable to have some good validation on these as we can
    use them at compile time to set up defaultable image fields.
    
    Co-authored-by: Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>

--- platform_umbrella/apps/common_core/lib/common_core/defaults/image.ex
+++ platform_umbrella/apps/common_core/lib/common_core/defaults/image.ex
@@ -2,9 +2,25 @@ defmodule CommonCore.Defaults.Image do
   @moduledoc false
   use CommonCore, :embedded_schema
 
+  @required_fields ~w(name tags default_tag)a
+
   batt_embedded_schema do
     field :name, :string
     field :tags, {:array, :string}
     field :default_tag, :string
+  end
+
+  @doc false
+  def changeset(image, attrs) do
+    image
+    |> CommonCore.Ecto.Schema.schema_changeset(attrs)
+    |> default_tag_in_tags()
+  end
+
+  def default_tag_in_tags(changeset) do
+    changeset
+    |> Ecto.Changeset.get_field(:tags, [])
+    |> Kernel.||([])
+    |> then(&Ecto.Changeset.validate_inclusion(changeset, :default_tag, &1))
   end
 end
