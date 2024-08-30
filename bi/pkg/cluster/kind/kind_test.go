92bf54083012c149364c2211cd3bfe70b174f2e6
Damian Peckett <damian@pecke.tt>
2024-08-30 13:23:31 +0200

    refactor: only spawn one kind cluster so we can avoid conflicts

--- bi/pkg/cluster/kind/kind_test.go
+++ bi/pkg/cluster/kind/kind_test.go
@@ -2,10 +2,11 @@ package kind_test
 
 import (
 	"bi/pkg/cluster/kind"
+	"bi/pkg/kube"
 	"bi/pkg/testutil"
 	"context"
-	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/neilotoole/slogt"
@@ -15,17 +16,53 @@ import (
 func TestKindClusterProvider(t *testing.T) {
 	testutil.IntegrationTest(t)
 
+	t.Log("Creating kind cluster")
+	clusterProvider := kind.NewClusterProvider(slogt.New(t), "bi-test", true)
+
 	ctx := context.Background()
-	logger := slogt.New(t)
+	require.NoError(t, clusterProvider.Init(ctx))
+	require.NoError(t, clusterProvider.Create(ctx, nil))
 
-	name := fmt.Sprintf("test-cluster-%d", os.Getpid())
-
-	p := kind.NewClusterProvider(logger, name, false)
-
-	require.NoError(t, p.Init(ctx))
-
-	require.NoError(t, p.Create(ctx, nil))
 	t.Cleanup(func() {
-		require.NoError(t, p.Destroy(ctx, nil))
+		t.Log("Deleting kind cluster")
+
+		require.NoError(t, clusterProvider.Destroy(ctx, nil))
 	})
+
+	outputDir := t.TempDir()
+
+	// Get a kubeconfig for the kind cluster (using its internal domain name).
+	kubeConfigPath := filepath.Join(outputDir, "kubeconfig")
+
+	kubeConfigFile, err := os.Create(kubeConfigPath)
+	require.NoError(t, err)
+
+	require.NoError(t, clusterProvider.KubeConfig(ctx, kubeConfigFile))
+	require.NoError(t, kubeConfigFile.Close())
+
+	clientConfPath := filepath.Join(outputDir, "client.yaml")
+	clientConfFile, err := os.OpenFile(clientConfPath, os.O_CREATE|os.O_WRONLY, 0o400)
+	require.NoError(t, err)
+
+	_, err = clusterProvider.WireGuardConfig(ctx, clientConfFile)
+	require.NoError(t, err)
+
+	require.NoError(t, clientConfFile.Close())
+
+	// Create a new kubernetes client that will send all requests over WireGuard.
+	kubeClient, err := kube.NewBatteryKubeClient(kubeConfigPath, clientConfPath)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		require.NoError(t, kubeClient.Close())
+	})
+
+	// Make sure we can communicate with the Kube API.
+	err = kubeClient.EnsureResourceExists(ctx, map[string]any{
+		"apiVersion": "v1",
+		"kind":       "Namespace",
+		"metadata": map[string]any{
+			"name": "test-namespace",
+		},
+	})
+	require.NoError(t, err)
 }
