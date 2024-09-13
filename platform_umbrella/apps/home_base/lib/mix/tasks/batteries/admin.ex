10ec1abaac6213d318da98450d7c6a7520484f43
Jason Maurer <jsonmaur@fastmail.com>
2024-09-13 12:01:30 -0600

    Handle repo errors and exit with correct status code on error

--- platform_umbrella/apps/home_base/lib/mix/tasks/batteries/admin.ex
+++ platform_umbrella/apps/home_base/lib/mix/tasks/batteries/admin.ex
@@ -13,23 +13,36 @@ defmodule Mix.Tasks.HomeBase.Batteries.Admin do
       [email] ->
         Mix.Task.run_in_apps("app.start", [:home_base])
 
-        if Accounts.get_user_by_email(email) do
-          team_ids = AdminTeams.admin_team_ids()
-          results = Enum.map(team_ids, &add_user_to_team(email, &1))
+        user = get_user(email)
+        team_ids = AdminTeams.admin_team_ids()
 
+        results = Enum.map(team_ids, &add_user_to_team(user.email, &1))
+        errors = Enum.filter(results, fn {status, result} -> unless(status == :ok, do: result) end)
+
+        if errors == [] do
           Logger.info("#{email} has been added as an admin to #{Enum.count(results)} team(s)")
         else
-          Logger.error("Could not find user for #{email}")
+          error("Something went wrong: #{inspect(errors)}")
         end
 
       _ ->
-        Logger.error("Please pass a user email as the first argument")
+        error("Please pass a user email as the first argument")
     end
+  end
+
+  defp get_user(email) do
+    Accounts.get_user_by_email(email) || error("Could not find a user for #{email}")
   end
 
   defp add_user_to_team(email, team_id) do
     team_id
     |> Teams.get_team!()
     |> Teams.create_team_role(%{invited_email: email, is_admin: true})
+  end
+
+  defp error(message) do
+    Logger.error(message)
+
+    exit({:shutdown, 1})
   end
 end
