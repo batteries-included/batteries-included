89677fe5f112aed2ed4a731dfed31bc0b47cffa0
Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>
2024-09-25 09:20:26 -0500

    script: multiple version support and wget support

--- platform_umbrella/apps/home_base_web/lib/home_base_web/controllers/installation_script_controller.ex
+++ platform_umbrella/apps/home_base_web/lib/home_base_web/controllers/installation_script_controller.ex
@@ -4,15 +4,16 @@ defmodule HomeBaseWeb.InstallScriptController do
 
   require EEx
 
-  EEx.function_from_file(:defp, :render_install_script, "priv/raw_files/install_script.sh", [:spec_url])
+  EEx.function_from_file(:defp, :render_install_script, "priv/raw_files/install_script.sh", [:spec_url, :version])
 
   action_fallback HomeBaseWeb.FallbackController
 
   def show(conn, %{"installation_id" => install_id}) do
     script =
-      conn
-      |> url(~p'/api/v1/installations/#{install_id}/spec')
-      |> render_install_script()
+      render_install_script(
+        url(conn, ~p'/api/v1/installations/#{install_id}/spec'),
+        CommonCore.Version.version()
+      )
 
     conn
     |> put_status(:ok)
--- platform_umbrella/apps/home_base_web/priv/raw_files/install_script.sh
+++ platform_umbrella/apps/home_base_web/priv/raw_files/install_script.sh
@@ -1,60 +1,103 @@
 #!/usr/bin/env sh
 
-set -eux
-
-BI="bi"
-TMP_DIR="$(mktemp -d)"
+set -eu
 
 INSTALL_SPEC_URL="<%= spec_url %>"
+VERSION_TAG="<%= version %>"
+
+BI="bi"
+EXT=".tar.gz"
+VERSION_LOC="${HOME}/.local/share/bi/${BI}-${VERSION_TAG}"
+
+TMP_DIR="$(mktemp -d)"
 
 trap 'rm -rf "${TMP_DIR}"' INT TERM EXIT
+
+check_bi_version() {
+    test -f "${VERSION_LOC}"
+}
 
 check_installed() {
     command -v "${1}" >/dev/null 2>&1
 }
 
-install_bi() {
+get_bi_release_url() {
     REPO="batteries-included/batteries-included"
-    INSTALL_DIR="$HOME/.local/bin"
     OS="$(uname -s)"
     ARCH="$(uname -m)"
-    EXT=".tar.gz"
+
+    fetch "https://api.github.com/repos/${REPO}/releases/tags/${VERSION_TAG}" -o - | grep "browser_download_url.*${OS}_${ARCH}${EXT}" | cut -d '"' -f 4
+}
+
+# borrowed heavily from nvm-sh/nvm/install.sh
+fetch() {
+    if check_installed "curl"; then
+        curl --fail --compressed --silent "$@"
+    elif check_installed "wget"; then
+        # Emulate curl with wget
+        ARGS=$(printf %s\\n "$*" "$@" 2>/dev/null | sed -e 's/--progress-bar /--progress=bar /' \
+            -e 's/--compressed //' \
+            -e 's/--fail //' \
+            -e 's/-L //' \
+            -e 's/-I /--server-response /' \
+            -e 's/-s /-q /' \
+            -e 's/-sS /-nv /' \
+            -e 's/-o /-O /' \
+            -e 's/-C - /-c /')
+        # shellcheck disable=SC2086
+        eval wget --quiet $ARGS
+    fi
+}
+
+download() {
+    DOWNLOAD_URL="$1"
+    OUTPUT_LOC="$2"
+
+    fetch -L -o "${OUTPUT_LOC}" "${DOWNLOAD_URL}"
+}
+
+install_release() {
+    SOURCE="$1"
+    OUTPUT="$2"
+    SYMLINK="$3"
+
+    chmod +x "${SOURCE}"
+    mkdir -p "$(dirname "${OUTPUT}")"
+    mkdir -p "$(dirname "${SYMLINK}")"
+    mv "${SOURCE}" "${OUTPUT}"
+    ln -sf "${OUTPUT}" "${SYMLINK}"
+}
+
+install_bi() {
+    INSTALL_DIR="${HOME}/.local/bin"
     OUTPUT_LOC="${TMP_DIR}/${BI}${EXT}"
 
-    VERSION=$(curl --silent "https://api.github.com/repos/${REPO}/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-    DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/${REPO}/releases/tags/${VERSION}" | grep "browser_download_url.*${OS}_${ARCH}${EXT}" | cut -d '"' -f 4)
+    DOWNLOAD_URL=$(get_bi_release_url)
 
-    if [ -z "${DOWNLOAD_URL}" ]; then
-        echo "Failed to find the asset for version ${VERSION}."
+    if [ -z "${DOWNLOAD_URL:-""}" ]; then
+        echo "Failed to find the asset for version ${VERSION_TAG}."
         exit 1
     fi
 
     echo "Downloading ${DOWNLOAD_URL} ..."
-    curl -L -o "${OUTPUT_LOC}" "${DOWNLOAD_URL}"
+    download "$DOWNLOAD_URL" "$OUTPUT_LOC"
 
     # extract to temp directory
-    if [ "$EXT" = ".tar.gz" ]; then
-        tar -xf "${OUTPUT_LOC}" -C "${TMP_DIR}"
-    elif [ "$EXT" = ".zip" ]; then
-        unzip "${OUTPUT_LOC}" -d "${TMP_DIR}"
-    fi
+    tar -xf "${OUTPUT_LOC}" -C "${TMP_DIR}"
 
-    chmod +x "${TMP_DIR}/${BI}"
-    mv "${TMP_DIR}/${BI}" "${INSTALL_DIR}/${BI}"
+    install_release "${TMP_DIR}/${BI}" "${VERSION_LOC}" "${INSTALL_DIR}/${BI}"
     echo "${BI} installed successfully to ${INSTALL_DIR}"
 }
 
 # install bi if needed first
+check_bi_version || install_bi
+check_installed "${BI}" || install_bi
+
 if check_installed "${BI}"; then
-    echo "${BI} already installed..."
+    :
 else
-    install_bi
-    if check_installed "${BI}"; then
-        :
-    else
-        echo "${BI} not installed correctly..."
-        exit 1
-    fi
+    echo "${BI} not installed correctly..."
+    exit 1
 fi
 
 # start install
