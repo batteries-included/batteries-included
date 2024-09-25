0b60190959674017ce439f35750815cafaa619ce
Elliott Clark <elliott@batteriesincl.com>
2024-09-25 13:57:21 -0500

    chore: bump the stable bi version now that 0.19.0 has been released
    Summary:
    0.19.0 should include enough that the script can work. So start using
    it.
    
    Test Plan:
    Deploy a new home base and try it out.

--- platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
+++ platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
@@ -2,5 +2,5 @@ defmodule CommonCore.Defaults.Versions do
   @moduledoc false
 
   @spec bi_stable_version() :: String.t()
-  def bi_stable_version, do: "0.18.1"
+  def bi_stable_version, do: "0.19.0"
 end
--- platform_umbrella/apps/common_core/lib/common_core/et/stable_versions_report.ex
+++ platform_umbrella/apps/common_core/lib/common_core/et/stable_versions_report.ex
@@ -4,5 +4,6 @@ defmodule CommonCore.ET.StableVersionsReport do
 
   batt_embedded_schema do
     field :control_server, :string, default: CommonCore.Defaults.Images.control_server_image()
+    field :bi, :string, default: CommonCore.Defaults.Versions.bi_stable_version()
   end
 end
