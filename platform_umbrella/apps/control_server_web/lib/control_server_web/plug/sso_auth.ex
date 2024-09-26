66005501bdbb56bebc7e19694779fc4c34222773
Jason Tarasovic <JTarasovic@users.noreply.github.com>
2024-09-26 14:37:58 -0500

    fix(home-base): clean up script and spec urls (#925)
    Tested locally and in AWS.
    
    ---------
    
    Co-authored-by: Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>

--- platform_umbrella/apps/control_server_web/lib/control_server_web/plug/sso_auth.ex
+++ platform_umbrella/apps/control_server_web/lib/control_server_web/plug/sso_auth.ex
@@ -1,6 +1,7 @@
 defmodule ControlServerWeb.Plug.SSOAuth do
   @moduledoc false
 
+  import CommonUI.UrlHelpers
   import Phoenix.Controller
   import Plug.Conn
 
@@ -39,7 +40,7 @@ defmodule ControlServerWeb.Plug.SSOAuth do
   end
 
   def redirect_to_oauth(conn) do
-    with {:ok, url} <- UserClient.authorize_url(UserClient, return_to: return_to(conn)) do
+    with {:ok, url} <- UserClient.authorize_url(UserClient, return_to: conn |> get_uri_from_conn() |> URI.to_string()) do
       conn
       |> redirect(external: url)
       |> halt()
@@ -47,36 +48,4 @@ defmodule ControlServerWeb.Plug.SSOAuth do
   end
 
   def battery_services_running?, do: KubeServices.Application.start_services?()
-
-  # request_url(conn) will always return http when running in cluster as TLS is terminated by istio / envoy
-  # so set scheme if x-forwarded-proto is set or ssl is enabled
-  defp return_to(conn) do
-    uri = conn |> request_url() |> URI.parse()
-    forward_proto = conn |> get_req_header("x-forwarded-proto") |> List.first()
-
-    scheme =
-      cond do
-        !is_nil(forward_proto) ->
-          forward_proto
-
-        SummaryBatteries.ssl_enabled?() ->
-          "https"
-
-        true ->
-          uri.scheme
-      end
-
-    uri |> determine_uri(scheme) |> URI.to_string()
-  end
-
-  defp determine_uri(uri, new_scheme)
-  defp determine_uri(%URI{} = uri, nil), do: uri
-  defp determine_uri(%URI{} = uri, ""), do: uri
-  defp determine_uri(%URI{scheme: old_scheme} = uri, new_scheme) when old_scheme == new_scheme, do: uri
-
-  defp determine_uri(%URI{scheme: "http", port: 80} = uri, "https"),
-    do: uri |> Map.put(:scheme, "https") |> Map.put(:port, 443)
-
-  # leave port alone if switching from http -> https and we're not on the default port - 80
-  defp determine_uri(%URI{scheme: "http"} = uri, "https"), do: Map.put(uri, :scheme, "https")
 end
--- platform_umbrella/apps/home_base_web/lib/home_base_web/controllers/installation_script_controller.ex
+++ platform_umbrella/apps/home_base_web/lib/home_base_web/controllers/installation_script_controller.ex
@@ -2,6 +2,8 @@ defmodule HomeBaseWeb.InstallScriptController do
   use HomeBaseWeb, :controller
   use HomeBaseWeb, :verified_routes
 
+  import CommonUI.UrlHelpers
+
   require EEx
 
   EEx.function_from_file(:defp, :render_install_script, "priv/raw_files/install_script.sh", [:spec_url, :version])
@@ -9,11 +11,10 @@ defmodule HomeBaseWeb.InstallScriptController do
   action_fallback HomeBaseWeb.FallbackController
 
   def show(conn, %{"installation_id" => install_id}) do
-    script =
-      render_install_script(
-        url(conn, ~p'/api/v1/installations/#{install_id}/spec'),
-        CommonCore.Defaults.Versions.bi_stable_version()
-      )
+    url = conn |> get_uri_from_conn() |> url(~p'/api/v1/installations/#{install_id}/spec')
+    bi_version = CommonCore.Defaults.Versions.bi_stable_version()
+
+    script = render_install_script(url, bi_version)
 
     conn
     |> put_status(:ok)
