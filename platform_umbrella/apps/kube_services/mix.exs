c44d4b0404416abdb539b982bf59bded8e39428c
Elliott Clark <elliott@batteriesincl.com>
2024-09-26 16:21:49 -0500

    chore: prepare for 0.22.0
    Summary:
    `bix set-version 0.22.0`
    
    Test Plan:
    CI

--- platform_umbrella/apps/kube_services/mix.exs
+++ platform_umbrella/apps/kube_services/mix.exs
@@ -4,7 +4,7 @@ defmodule KubeServices.MixProject do
   def project do
     [
       app: :kube_services,
-      version: "0.21.0",
+      version: "0.22.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- platform_umbrella/mix.exs
+++ platform_umbrella/mix.exs
@@ -4,7 +4,7 @@ defmodule ControlServer.Umbrella.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.21.0",
+      version: "0.22.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: releases(),
