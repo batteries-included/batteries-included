6d2b29a354777e7717accdf032ac4c64f1121220
Elliott <elliott@batteriesincl.com>
2024-09-27 10:18:35 -0500

    chore: prepare for 0.22.0 (#939)
    Summary:
    `bix set-version 0.22.0`
    
    Test Plan:
    CI

--- platform_umbrella/apps/common_core/mix.exs
+++ platform_umbrella/apps/common_core/mix.exs
@@ -4,7 +4,7 @@ defmodule CommonCore.MixProject do
   def project do
     [
       app: :common_core,
-      version: "0.21.0",
+      version: "0.22.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
