d7da2df2443316bfb993eb156bd6070dafabe824
Elliott Clark <elliott@batteriesincl.com>
2024-10-08 17:29:14 -0500

    chore: prepare for 0.24.0
    Summary:
    Quick release to see if you can tag non-dependabot commits.
    
    Test Plan:
    Tag github's it

--- platform_umbrella/apps/event_center/mix.exs
+++ platform_umbrella/apps/event_center/mix.exs
@@ -4,7 +4,7 @@ defmodule EventCenter.MixProject do
   def project do
     [
       app: :event_center,
-      version: "0.23.0",
+      version: "0.24.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
