b9806b60b88abc8277802dcdc5dfef09d6f239a5
Elliott <elliott@batteriesincl.com>
2024-10-15 10:27:16 -0400

    chore: prepare for 0.26.0 (#1051)
    Summary:
    0.25.0 should be good to go
    
    Test Plan:
    Push 0.25 with the last docs changes and tag the things.

--- platform_umbrella/apps/common_ui/mix.exs
+++ platform_umbrella/apps/common_ui/mix.exs
@@ -4,7 +4,7 @@ defmodule CommonUI.MixProject do
   def project do
     [
       app: :common_ui,
-      version: "0.25.0",
+      version: "0.26.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
