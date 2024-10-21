01ec28b6b95e2e5538c716ab757d477c3cc8c606
Elliott <elliott@batteriesincl.com>
2024-10-21 09:37:45 -0500

    chore: prepare for 0.27.0 (#1077)
    Summary:
    We've done a lot lately. Time for a release
    
    Test Plan:
    CI

--- platform_umbrella/mix.exs
+++ platform_umbrella/mix.exs
@@ -4,7 +4,7 @@ defmodule ControlServer.Umbrella.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.26.0",
+      version: "0.27.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: releases(),
