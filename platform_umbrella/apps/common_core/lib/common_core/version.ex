b7e43a9a668b9c0dffca4f0e28e524e29da69b83
Elliott Clark <elliott@batteriesincl.com>
2024-10-24 11:17:02 -0500

    fix: conditionally add probes to controlserver
    Summary:
    Stable versions don't have probes
    
    Test Plan:
    CI

--- platform_umbrella/apps/common_core/lib/common_core/version.ex
+++ platform_umbrella/apps/common_core/lib/common_core/version.ex
@@ -19,4 +19,37 @@ defmodule CommonCore.Version do
 
   @spec hash() :: String.t()
   def hash, do: @hash
+
+  @spec compare(String.t(), String.t()) ::
+          {:ok, :equal} | {:ok, :greater} | {:ok, :lesser} | {:error, :incomparable}
+  def compare(a, b) do
+    inner_compare(parse(a), parse(b))
+  end
+
+  # Parse a dotted version string into a list of integers
+  @spec parse(String.t()) :: [integer()]
+  defp parse(str) do
+    str
+    |> String.split(".")
+    |> Enum.map(&parse_integer/1)
+  end
+
+  # This parses the integer part of dotted version strings
+  # The last part can include a dash and a hash or other suffix
+  # This function will only parse the integer part
+  @spec parse_integer(String.t()) :: integer()
+  defp parse_integer(str) do
+    str
+    |> String.split("-")
+    |> List.first()
+    |> String.to_integer()
+  end
+
+  @spec inner_compare([integer()], [integer()]) ::
+          {:ok, :equal} | {:ok, :greater} | {:ok, :lesser} | {:error, :incomparable}
+  defp inner_compare([], []), do: {:ok, :equal}
+  defp inner_compare([a | as], [b | bs]) when a == b, do: inner_compare(as, bs)
+  defp inner_compare([a | _], [b | _]) when a > b, do: {:ok, :greater}
+  defp inner_compare([a | _], [b | _]) when a < b, do: {:ok, :lesser}
+  defp inner_compare(_, _), do: {:error, :incomparable}
 end
