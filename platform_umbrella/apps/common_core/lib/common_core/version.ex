eaa0566b0237e0bd5bd99e635aaff3a0a3adfb4b
Jason Tarasovic <JTarasovic@users.noreply.github.com>
2024-10-29 15:01:10 -0500

    fix: don't crash on incomparable versions (#1135)
    Trying to use an override version runs into issues when comparing
    version. This fixes it.
    
    Co-authored-by: Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>

--- platform_umbrella/apps/common_core/lib/common_core/version.ex
+++ platform_umbrella/apps/common_core/lib/common_core/version.ex
@@ -27,7 +27,7 @@ defmodule CommonCore.Version do
   end
 
   # Parse a dotted version string into a list of integers
-  @spec parse(String.t()) :: [integer()]
+  @spec parse(String.t()) :: [integer() | nil]
   defp parse(str) do
     str
     |> String.split(".")
@@ -37,17 +37,27 @@ defmodule CommonCore.Version do
   # This parses the integer part of dotted version strings
   # The last part can include a dash and a hash or other suffix
   # This function will only parse the integer part
-  @spec parse_integer(String.t()) :: integer()
+  @spec parse_integer(String.t()) :: integer() | nil
   defp parse_integer(str) do
-    str
-    |> String.split("-")
-    |> List.first()
-    |> String.to_integer()
+    result =
+      str
+      |> String.split("-")
+      |> List.first()
+      |> Integer.parse()
+
+    case result do
+      {int, _} ->
+        int
+
+      :error ->
+        nil
+    end
   end
 
-  @spec inner_compare([integer()], [integer()]) ::
+  @spec inner_compare([integer() | nil], [integer() | nil]) ::
           {:ok, :equal} | {:ok, :greater} | {:ok, :lesser} | {:error, :incomparable}
   defp inner_compare([], []), do: {:ok, :equal}
+  defp inner_compare([a | _], [b | _]) when is_nil(a) or is_nil(b), do: {:error, :incomparable}
   defp inner_compare([a | as], [b | bs]) when a == b, do: inner_compare(as, bs)
   defp inner_compare([a | _], [b | _]) when a > b, do: {:ok, :greater}
   defp inner_compare([a | _], [b | _]) when a < b, do: {:ok, :lesser}
