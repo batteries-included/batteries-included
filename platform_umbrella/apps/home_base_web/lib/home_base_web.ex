e68f5519ac2a224e92e5e133fba39c2fe7d4b066
Elliott Clark <elliott@batteriesincl.com>
2024-11-01 19:17:28 -0500

    fix: formatting

--- platform_umbrella/apps/home_base_web/lib/home_base_web.ex
+++ platform_umbrella/apps/home_base_web/lib/home_base_web.ex
@@ -16,7 +16,6 @@ defmodule HomeBaseWeb do
   below. Instead, define additional modules and import
   those modules here.
   """
-  alias ControlServerWeb.Common
 
   def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
 
