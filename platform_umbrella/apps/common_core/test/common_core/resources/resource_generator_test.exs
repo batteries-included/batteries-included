778cb59430cef181bab204cec87888bece48f731
Elliott Clark <elliott@batteriesincl.com>
2024-11-01 19:13:12 -0500

    refactor: add type specs to resource generator
    Summary:
    Types and Test changes needed for it.
    
    Test Plan:
    CI
    Tests updated after failing the new stricter guards

--- platform_umbrella/apps/common_core/test/common_core/resources/resource_generator_test.exs
+++ platform_umbrella/apps/common_core/test/common_core/resources/resource_generator_test.exs
@@ -1,16 +1,18 @@
 defmodule CommonCore.Resources.ResourceGeneratorTest do
   use ExUnit.Case
 
+  import CommonCore.Factory
+
   alias CommonCore.Resources.ExampleGenerator
 
   describe "CommonCore.Resources.ResourceGenerator" do
     test "Test the generator works" do
-      result = ExampleGenerator.materialize(%{config: %{}}, %{})
+      result = ExampleGenerator.materialize(%{config: %{}}, build(:state_summary))
       assert %{"/service_account/main" => _} = result
     end
 
     test "The multi resource with a list works" do
-      result = ExampleGenerator.materialize(%{config: %{}}, %{})
+      result = ExampleGenerator.materialize(%{config: %{}}, build(:state_summary))
 
       assert %{
                "/service_account/multi_list_0" => _,
@@ -27,7 +29,7 @@ defmodule CommonCore.Resources.ResourceGeneratorTest do
     end
 
     test "The multi resource with a map" do
-      result = ExampleGenerator.materialize(%{config: %{}}, %{})
+      result = ExampleGenerator.materialize(%{config: %{}}, build(:state_summary))
 
       assert %{
                "/map_0/service_account/map_0" => _,
