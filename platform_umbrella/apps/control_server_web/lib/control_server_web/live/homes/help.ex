0da45283b705c105db84bb97865c58dc65d0802e
Elliott Clark <elliott@batteriesincl.com>
2024-11-07 16:06:27 -0600

    feat: add version to control_server_web help.ex
    Summary:
    This makes the help pages follow each other in style.
    
    Test Plan:
    Visual

--- platform_umbrella/apps/control_server_web/lib/control_server_web/live/homes/help.ex
+++ platform_umbrella/apps/control_server_web/lib/control_server_web/live/homes/help.ex
@@ -16,7 +16,7 @@ defmodule ControlServerWeb.Live.Help do
   def render(assigns) do
     ~H"""
     <.flex column class="items-center justify-center h-full text-gray-darkest dark:text-gray-lighter">
-      <.icon name={:question_mark_circle} class="size-60 text-primary opacity-15" />
+      <.icon name={:question_mark_circle} class="size-60 text-primary opacity-35" />
       <.h2>Need Some Help?</.h2>
 
       <div class="max-w-lg">
@@ -25,11 +25,23 @@ defmodule ControlServerWeb.Live.Help do
         </p>
 
         <div class="flex gap-4">
-          <.a variant="bordered-lg" icon={:book_open} href={docs_url()} target="_blank">Docs</.a>
+          <.a
+            variant="bordered-lg"
+            icon={:book_open}
+            href={docs_url()}
+            target="_blank"
+            class="opacity-35 hover:opacity-100"
+          >
+            Docs
+          </.a>
           <.a variant="bordered-lg" icon={:github} href={github_url()} target="_blank">GitHub</.a>
           <.a variant="bordered-lg" icon={:slack} href={slack_url()} target="_blank">Slack</.a>
         </div>
       </div>
+      <.flex class="mt-auto items-center justify-around text-gray">
+        <span><%= CommonCore.Version.version() %></span>
+        <span><%= CommonCore.Version.hash() %></span>
+      </.flex>
     </.flex>
     """
   end
