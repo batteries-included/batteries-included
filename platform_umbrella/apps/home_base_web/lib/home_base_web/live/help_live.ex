7e0a4f68fbbc46520823327d9bc693cf0083e30d
Elliott <elliott@batteriesincl.com>
2024-11-07 16:04:58 -0600

    feat: add a version id to home base (#1168)
    Summary:
    I updated and wanted to check that it was updated before hitting delete.
    However there wasn't a way. So I added this
    
    Test Plan:
    Visual
    
    
    ![image](https://github.com/user-attachments/assets/7fad5c81-9c0a-4304-bfe0-40c5e7d00d42)
    
    ---------
    
    Co-authored-by: Jason Tarasovic <JTarasovic@users.noreply.github.com>

--- platform_umbrella/apps/home_base_web/lib/home_base_web/live/help_live.ex
+++ platform_umbrella/apps/home_base_web/lib/home_base_web/live/help_live.ex
@@ -13,8 +13,8 @@ defmodule HomeBaseWeb.HelpLive do
 
   def render(assigns) do
     ~H"""
-    <div class="flex flex-col items-center justify-center min-h-full">
-      <.icon name={:question_mark_circle} class="size-60 mx-auto text-primary opacity-15" />
+    <.flex column class="items-center justify-center">
+      <.icon name={:question_mark_circle} class="size-60 mx-auto text-primary opacity-35" />
       <.h2 class="mb-8">Need Some Help?</.h2>
 
       <div class="max-w-lg">
@@ -22,13 +22,25 @@ defmodule HomeBaseWeb.HelpLive do
           Batteries Included is still in beta, so we'd love your feedback! Feel free to check out the docs, open an issue on GitHub, or just drop into Slack to say hey.
         </p>
 
-        <div class="flex gap-4">
-          <.a variant="bordered-lg" icon={:book_open} href={docs_url()} target="_blank">Docs</.a>
+        <.flex>
+          <.a
+            variant="bordered-lg"
+            icon={:book_open}
+            href={docs_url()}
+            target="_blank"
+            class="opacity-35 hover:opacity-100"
+          >
+            Docs
+          </.a>
           <.a variant="bordered-lg" icon={:github} href={github_url()} target="_blank">GitHub</.a>
           <.a variant="bordered-lg" icon={:slack} href={slack_url()} target="_blank">Slack</.a>
-        </div>
+        </.flex>
       </div>
-    </div>
+      <.flex class="mt-auto items-center justify-around text-gray">
+        <span><%= CommonCore.Version.version() %></span>
+        <span><%= CommonCore.Version.hash() %></span>
+      </.flex>
+    </.flex>
     """
   end
 end
