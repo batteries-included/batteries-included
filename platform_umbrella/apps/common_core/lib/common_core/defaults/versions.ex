1a6aac2b800e7f722893845e2ec3a0e3619901a4
Elliott <elliott@batteriesincl.com>
2024-11-07 11:30:42 -0600

    chore: update bi stable  version (#1164)
    Summary:
    Update to the latest released version
    
    Test Plan:
    Ci

--- platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
+++ platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
@@ -2,5 +2,5 @@ defmodule CommonCore.Defaults.Versions do
   @moduledoc false
 
   @spec bi_stable_version() :: String.t()
-  def bi_stable_version, do: "0.24.0"
+  def bi_stable_version, do: "0.31.0"
 end
