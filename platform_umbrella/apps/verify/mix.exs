9f93ef63e0ee4a411f55b660da65c54519f9e102
Elliott <elliott@batteriesincl.com>
2024-11-11 13:46:02 -0600

    chore: prepare for 0.34.0 (#1184)
    Summary:
    - Set version 0.34.0
    
    Test Plan:
    - CI

--- platform_umbrella/apps/verify/mix.exs
+++ platform_umbrella/apps/verify/mix.exs
@@ -4,7 +4,7 @@ defmodule Verify.MixProject do
   def project do
     [
       app: :verify,
-      version: "0.33.0",
+      version: "0.34.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
