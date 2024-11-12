6f1631f880bce639cefd94f5a1a8a5c9d953c797
Jason Tarasovic <JTarasovic@users.noreply.github.com>
2024-11-12 17:45:28 -0600

    feat(bix): prevent running control server inadvertantly (#1197)

--- platform_umbrella/apps/kube_services/lib/kube_services.ex
+++ platform_umbrella/apps/kube_services/lib/kube_services.ex
@@ -2,16 +2,4 @@ defmodule KubeServices do
   @moduledoc """
   Documentation for `KubeServices`.
   """
-
-  def get_current_events do
-    conn = CommonCore.ConnectionPool.get!()
-
-    operation = K8s.Client.list("v1", "Event")
-
-    with {:ok, result} <- K8s.Client.run(conn, operation) do
-      result
-    end
-  end
-
-  def cluster_type, do: Application.get_env(:kube_services, :cluster_type, :dev)
 end
