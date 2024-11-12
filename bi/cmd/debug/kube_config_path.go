42276d00979484c2987f4de01847e882c6dcdc66
Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>
2024-11-12 14:08:43 -0600

    feat(bi): add debug command for getting kube config file

---
+++ bi/cmd/debug/kube_config_path.go
@@ -0,0 +1,33 @@
+package debug
+
+import (
+	"fmt"
+
+	"bi/pkg/installs"
+	"bi/pkg/log"
+
+	"github.com/spf13/cobra"
+)
+
+var kubeConfigPath = &cobra.Command{
+	Use:   "kube-config-path [install-slug|install-spec-url|install-spec-file]",
+	Short: "Print the path to the install's kube config",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		env, err := installs.NewEnv(cmd.Context(), args[0])
+		if err != nil {
+			return err
+		}
+
+		if err := log.CollectDebugLogs(env.DebugLogPath(cmd.CommandPath())); err != nil {
+			return err
+		}
+
+		fmt.Print(env.KubeConfigPath())
+
+		return nil
+	},
+}
+
+func init() {
+	debugCmd.AddCommand(kubeConfigPath)
+}
