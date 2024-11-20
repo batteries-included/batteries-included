c358d3fe8e1316fb00469933f6d2f0eabb133ebc
Elliott <elliott@batteriesincl.com>
2024-11-20 13:15:45 -0600

    style: show pg cluster type on abbridged tables (#1237)
    Summary:
    This is based on user feedback. They wanted to know what type of
    cluster, so they knew what to edit/what's safe.
    
    
    ![image](https://github.com/user-attachments/assets/d8205335-f65e-452e-8906-8cd978f8c03e)
    
    
    Test Plan:
    tests included

---
+++ platform_umbrella/apps/control_server_web/test/components/postgres_table_test.exs
@@ -0,0 +1,42 @@
+defmodule ControlServerWeb.Components.PostgresClustersTableTest do
+  use Heyya.SnapshotCase
+
+  import ControlServer.Factory
+  import ControlServerWeb.PostgresClusterTable
+
+  describe "postgres_clusters_table" do
+    component_snapshot_test "abbridgred" do
+      assigns = %{
+        clusters: [
+          :postgres_cluster
+          |> build(type: :standard, name: "cluster1")
+          |> Map.put(:id, "00-00-00-00-00-00-00-00-00-00-01"),
+          :postgres_cluster
+          |> build(type: :internal, name: "cluster2")
+          |> Map.put(:id, "00-00-00-00-00-00-00-00-00-00-02")
+        ]
+      }
+
+      ~H"""
+      <.postgres_clusters_table rows={@clusters} abbridged />
+      """
+    end
+
+    component_snapshot_test "unabbridged" do
+      assigns = %{
+        clusters: [
+          :postgres_cluster
+          |> build(type: :standard, name: "cluster1", num_instances: 3)
+          |> Map.put(:id, "00-00-00-00-00-00-00-00-00-00-03"),
+          :postgres_cluster
+          |> build(type: :internal, name: "cluster2", num_instances: 1)
+          |> Map.put(:id, "00-00-00-00-00-00-00-00-00-00-04")
+        ]
+      }
+
+      ~H"""
+      <.postgres_clusters_table rows={@clusters} />
+      """
+    end
+  end
+end
