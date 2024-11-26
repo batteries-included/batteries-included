a1b0f095c35c705e5e7818de265f05529dc91a18
Elliott Clark <elliott@batteriesincl.com>
2024-11-26 13:43:06 -0600

    chore: prepare for 0.39.0
    Summary:
    After keycloak is fixed.
    
    Test Plan:
    CI

--- platform_umbrella/apps/common_ui/mix.exs
+++ platform_umbrella/apps/common_ui/mix.exs
@@ -4,7 +4,7 @@ defmodule CommonUI.MixProject do
   def project do
     [
       app: :common_ui,
-      version: "0.38.0",
+      version: "0.39.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
