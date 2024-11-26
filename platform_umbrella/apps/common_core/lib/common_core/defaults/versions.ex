84552816ef126678136203e010996e9eb4a266a0
Elliott <elliott@batteriesincl.com>
2024-11-26 11:12:03 -0600

    chore: update stable version of bi (#1260)
    Summary:
    Latest version released is 0.37.0 so bump to that
    
    Test Plan:
    Ci

--- platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
+++ platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
@@ -2,5 +2,5 @@ defmodule CommonCore.Defaults.Versions do
   @moduledoc false
 
   @spec bi_stable_version() :: String.t()
-  def bi_stable_version, do: "0.35.0"
+  def bi_stable_version, do: "0.37.0"
 end
