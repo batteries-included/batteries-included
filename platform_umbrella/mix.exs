c5b09ab5e55bcc4f86a07c0cd754b982a8be795a
Elliott <elliott@batteriesincl.com>
2024-11-26 13:53:58 -0600

    chore: prepare for 0.39.0 (#1269)
    Summary:
    After keycloak is fixed.
    
    Test Plan:
    CI

--- platform_umbrella/mix.exs
+++ platform_umbrella/mix.exs
@@ -4,7 +4,7 @@ defmodule ControlServer.Umbrella.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.38.0",
+      version: "0.39.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: releases(),
