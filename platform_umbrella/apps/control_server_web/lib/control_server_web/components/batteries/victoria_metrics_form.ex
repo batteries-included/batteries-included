f73f3ba78c479aa8a44b306fc107cd9f72f4b7fd
Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>
2024-12-05 14:12:19 -0600

    feat(monitoring): use sliders and presets for victoria metrics storage

--- platform_umbrella/apps/control_server_web/lib/control_server_web/components/batteries/victoria_metrics_form.ex
+++ platform_umbrella/apps/control_server_web/lib/control_server_web/components/batteries/victoria_metrics_form.ex
@@ -8,7 +8,49 @@ defmodule ControlServerWeb.Batteries.VictoriaMetricsForm do
   alias CommonCore.Batteries.VictoriaMetricsConfig
   alias CommonCore.Util.Memory
 
+  def handle_event(
+        "change_storage_size_range",
+        %{"victoria_metrics_config" => %{"virtual_vmselect_storage_size_range" => size}},
+        socket
+      ),
+      do: update_volume_size(:vmselect_volume_size, size, socket)
+
+  def handle_event(
+        "change_storage_size_range",
+        %{"battery_config" => %{"virtual_vmselect_storage_size_range" => size}},
+        socket
+      ),
+      do: update_volume_size(:vmselect_volume_size, size, socket)
+
+  def handle_event(
+        "change_storage_size_range",
+        %{"victoria_metrics_config" => %{"virtual_vmstorage_storage_size_range" => size}},
+        socket
+      ),
+      do: update_volume_size(:vmstorage_volume_size, size, socket)
+
+  def handle_event(
+        "change_storage_size_range",
+        %{"battery_config" => %{"virtual_vmstorage_storage_size_range" => size}},
+        socket
+      ),
+      do: update_volume_size(:vmstorage_volume_size, size, socket)
+
+  defp update_volume_size(field, size, %{assigns: %{form: %{name: name, source: source}}} = socket) do
+    old_size = Map.get(source.data, field, 0) || 0
+
+    changeset =
+      source
+      |> VictoriaMetricsConfig.put_volume_size(field, size, old_size)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, form: to_form(changeset, as: name))}
+  end
+
   def render(assigns) do
+    assigns =
+      assign(assigns, :ticks, VictoriaMetricsConfig.volume_range_ticks())
+
     ~H"""
     <div class="contents">
       <.panel title="Description">
@@ -82,6 +124,25 @@ defmodule ControlServerWeb.Batteries.VictoriaMetricsForm do
                 <:note>You can't reduce this once it has been created.</:note>
                 <.input type="number" field={@form[:vmselect_volume_size]} debounce={false} />
               </.field>
+              <.field>
+                <.input
+                  id="vmselect_virtual_volume_size"
+                  type="range"
+                  field={@form[:virtual_vmselect_storage_size_range]}
+                  show_value={false}
+                  min={@ticks |> Memory.min_range_value()}
+                  max={@ticks |> Memory.max_range_value()}
+                  ticks={@ticks}
+                  tick_target={@myself}
+                  tick_click="change_storage_size_range"
+                  phx-change="change_storage_size_range"
+                  phx-target={@myself}
+                  lower_boundary={
+                    @form.data.vmselect_volume_size |> Memory.bytes_to_range_value(@ticks)
+                  }
+                  class="px-5 self-center"
+                />
+              </.field>
             </.fieldset>
 
             <.fieldset>
@@ -91,6 +152,25 @@ defmodule ControlServerWeb.Batteries.VictoriaMetricsForm do
                 </:label>
                 <:note>You can't reduce this once it has been created.</:note>
                 <.input type="number" field={@form[:vmstorage_volume_size]} debounce={false} />
+              </.field>
+              <.field>
+                <.input
+                  id="vmstorage_virtual_volume_size"
+                  type="range"
+                  field={@form[:virtual_vmstorage_storage_size_range]}
+                  show_value={false}
+                  min={@ticks |> Memory.min_range_value()}
+                  max={@ticks |> Memory.max_range_value()}
+                  ticks={@ticks}
+                  tick_target={@myself}
+                  tick_click="change_storage_size_range"
+                  phx-change="change_storage_size_range"
+                  phx-target={@myself}
+                  lower_boundary={
+                    @form.data.vmstorage_volume_size |> Memory.bytes_to_range_value(@ticks)
+                  }
+                  class="px-5 self-center"
+                />
               </.field>
             </.fieldset>
           <% end %>
