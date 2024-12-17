252f96540dd96109b0d883ab777d836a11161def
Elliott Clark <elliott@batteriesincl.com>
2024-12-17 14:07:08 -0600

    chore: prepare for 0.43.0
    Summary:
    Bump version
    Release so that the 0.42.0 release can be used to bring up home base.
    
    Test Plan:
    - Ci

--- platform_umbrella/apps/home_base_web/mix.exs
+++ platform_umbrella/apps/home_base_web/mix.exs
@@ -4,7 +4,7 @@ defmodule HomeBaseWeb.MixProject do
   def project do
     [
       app: :home_base_web,
-      version: "0.42.0",
+      version: "0.43.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
