b0da51a618fb2b26f42af2b4ea29acfbf64982a9
Elliott <elliott@batteriesincl.com>
2024-12-17 14:15:43 -0600

    chore: prepare for 0.43.0 (#1360)
    Summary:
    Bump version
    Release so that the 0.42.0 release can be used to bring up home base.
    
    Test Plan:
    - CI

--- platform_umbrella/apps/kube_services/mix.exs
+++ platform_umbrella/apps/kube_services/mix.exs
@@ -4,7 +4,7 @@ defmodule KubeServices.MixProject do
   def project do
     [
       app: :kube_services,
-      version: "0.42.0",
+      version: "0.43.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
