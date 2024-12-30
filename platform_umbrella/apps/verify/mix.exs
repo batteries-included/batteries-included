cc021e8f1c11b16d00b6079342096f3444240af4
Elliott Clark <elliott@batteriesincl.com>
2024-12-30 13:48:05 -0600

    chore: prepare for 0.47.0
    Summary:
    - Getting ready to release 0.46.0
    
    Test Plan:
    - Ci

--- platform_umbrella/apps/verify/mix.exs
+++ platform_umbrella/apps/verify/mix.exs
@@ -4,7 +4,7 @@ defmodule Verify.MixProject do
   def project do
     [
       app: :verify,
-      version: "0.46.0",
+      version: "0.47.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
