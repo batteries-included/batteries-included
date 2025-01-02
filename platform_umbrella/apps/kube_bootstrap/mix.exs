e9d287091cf17309a021c3d0758e4c8d5d0c0fd2
Elliott Clark <elliott@batteriesincl.com>
2025-01-02 10:48:13 -0600

    chore: prepare for 0.48.0
    Summary:
    - Release with the bumped bi version
    
    Test Plan:
    - CI

--- platform_umbrella/apps/kube_bootstrap/mix.exs
+++ platform_umbrella/apps/kube_bootstrap/mix.exs
@@ -4,7 +4,7 @@ defmodule KubeBootstrap.MixProject do
   def project do
     [
       app: :kube_bootstrap,
-      version: "0.47.0",
+      version: "0.48.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
