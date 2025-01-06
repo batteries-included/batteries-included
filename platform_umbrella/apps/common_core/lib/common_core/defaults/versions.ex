cde2c8550864446f8dcdc3cb445a2065b467d127
Elliott Clark <elliott@batteriesincl.com>
2025-01-06 16:18:06 -0600

    chore: bump bi version
    Summary:
    - Try and bump bi version
    
    Test Plan:
    - CI

--- platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
+++ platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
@@ -2,5 +2,5 @@ defmodule CommonCore.Defaults.Versions do
   @moduledoc false
 
   @spec bi_stable_version() :: String.t()
-  def bi_stable_version, do: "0.46.0"
+  def bi_stable_version, do: "0.47.0"
 end
