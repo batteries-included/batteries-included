5831a16addd790a8c5050c75cec234821dffb9d7
Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>
2025-01-30 15:03:04 -0600

    chore: prepare for release

--- platform_umbrella/mix.exs
+++ platform_umbrella/mix.exs
@@ -4,7 +4,7 @@ defmodule ControlServer.Umbrella.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.50.0",
+      version: "0.51.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: releases(),
