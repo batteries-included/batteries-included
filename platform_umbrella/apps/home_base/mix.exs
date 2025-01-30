7e8c30652856f96bb3f5184e02e3c1e368227d1b
Elliott Clark <elliott@batteriesincl.com>
2025-01-30 17:50:52 -0600

    chore: prepare for 0.52.0
    Summary:
    - We should have changelogs.
    - Releasing 0.51.0 should exactly follow the doc
    
    Test Plan:
    - CI

--- platform_umbrella/apps/home_base/mix.exs
+++ platform_umbrella/apps/home_base/mix.exs
@@ -4,7 +4,7 @@ defmodule HomeBase.MixProject do
   def project do
     [
       app: :home_base,
-      version: "0.51.0",
+      version: "0.52.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
