857793a45141ed927f6b4a4e38dad3fc0f4b5f37
Elliott <elliott@batteriesincl.com>
2025-02-03 13:50:30 -0600

    chore: prepare for 0.54.0 (#1564)
    Summary:
    - Bump the version to start 0.54.0
    
    Test Plan:
    - CI
    - Local testing

--- platform_umbrella/apps/kube_bootstrap/mix.exs
+++ platform_umbrella/apps/kube_bootstrap/mix.exs
@@ -4,7 +4,7 @@ defmodule KubeBootstrap.MixProject do
   def project do
     [
       app: :kube_bootstrap,
-      version: "0.53.0",
+      version: "0.54.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
