1130bf2fe9ca54c6299deae32af6dc622c907d2c
Elliott Clark <elliott@batteriesincl.com>
2025-02-04 14:33:31 -0600

    chore: prepare for 0.55.0
    Summary:
    - Bump the version
    
    Test Plan:
    - CI

--- platform_umbrella/apps/kube_services/mix.exs
+++ platform_umbrella/apps/kube_services/mix.exs
@@ -4,7 +4,7 @@ defmodule KubeServices.MixProject do
   def project do
     [
       app: :kube_services,
-      version: "0.54.0",
+      version: "0.55.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
