a96e15c86c86b52e6d999afa285c65817418639b
Jason Tarasovic <JTarasovic@users.noreply.github.com>
2025-02-18 13:26:08 -0600

    chore(karpenter): enable gpu support for nvidia nodes (#1638)
    The AL2 AMIs don't have default support for GPUs but the bottlerocket
    AMIs do. So create a nodeclass for it and use that class for the nvidia
    node pool.
    
    Co-authored-by: Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>

--- platform_umbrella/apps/common_core/lib/common_core/batteries/karpenter_config.ex
+++ platform_umbrella/apps/common_core/lib/common_core/batteries/karpenter_config.ex
@@ -12,5 +12,6 @@ defmodule CommonCore.Batteries.KarpenterConfig do
     field :node_role_name, :string
 
     defaultable_field :ami_alias, :string, default: "al2@v20250212"
+    defaultable_field :bottlerocket_ami_alias, :string, default: "bottlerocket@v1.32.0"
   end
 end
