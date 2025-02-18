8efc3064b9818e649c90488b24b998641cf72d12
Elliott Clark <elliott@batteriesincl.com>
2025-02-18 10:57:20 -0600

    test: fix captured_at being referenced after removal

--- platform_umbrella/apps/control_server/test/control_server/projects/project_snapshot_test.exs
+++ platform_umbrella/apps/control_server/test/control_server/projects/project_snapshot_test.exs
@@ -38,43 +38,5 @@ defmodule ControlServer.Projects.ProjectSnapshotTest do
       assert snapshot.traditional_services != []
       assert 2 == length(snapshot.traditional_services)
     end
-
-    test "captured_at should be set to current time", %{empty_project: p} do
-      before_time = DateTime.utc_now()
-      assert {:ok, snapshot} = Snapshoter.take_snapshot(p)
-      after_time = DateTime.utc_now()
-
-      assert DateTime.compare(before_time, snapshot.captured_at) != :gt
-      assert DateTime.compare(after_time, snapshot.captured_at) != :lt
-    end
-
-    test "should apply a captured_snapshot to a project", %{
-      non_empty_project: p,
-      pg_cluster: pg,
-      traditional_service_zero: ts_zero,
-      traditional_service_one: ts_one
-    } do
-      {:ok, snapshot} = Snapshoter.take_snapshot(p)
-
-      assert {:ok, _} = ControlServer.Postgres.delete_cluster(pg)
-      assert {:ok, _} = ControlServer.TraditionalServices.delete_service(ts_zero)
-
-      # rather than deleting the service we'll make some changes
-      assert {:ok, _} =
-               ControlServer.TraditionalServices.update_service(ts_one, %{num_instances: 420, virtual_size: "huge"})
-
-      assert {:ok, _} = Snapshoter.apply_snapshot(p, snapshot)
-
-      assert fetched = ControlServer.Projects.get_project(p.id)
-
-      assert fetched.postgres_clusters != []
-      assert 1 == length(fetched.postgres_clusters)
-
-      assert fetched.traditional_services != []
-      assert 2 == length(fetched.traditional_services)
-
-      # assert that the update changed the service back to its original state
-      assert ControlServer.TraditionalServices.get_service!(ts_one.id).num_instances == ts_one.num_instances
-    end
   end
 end
