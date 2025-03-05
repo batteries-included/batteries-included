cf5e0c3e90a94f37e94b9aa2d2c87f1d64e4bd4e
Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>
2025-03-05 12:11:41 -0600

    chore(ollama): refactor models and clean up

--- platform_umbrella/apps/common_core/lib/common_core/ollama/model_instance.ex
+++ platform_umbrella/apps/common_core/lib/common_core/ollama/model_instance.ex
@@ -40,18 +40,18 @@ defmodule CommonCore.Ollama.ModelInstance do
     }
   ]
 
-  @models [
-    [key: "Deepseek R1 1.5b", value: "deepseek-r1:1.5b", size: Memory.to_bytes(1.1, :GB)],
-    [key: "Deepseek R1 7b", value: "deepseek-r1:7b", size: Memory.to_bytes(4.7, :GB)],
-    [key: "Llama 3.2 3b", value: "llama3.2:3b", size: Memory.to_bytes(2.0, :GB)],
-    [key: "Llama 3.2 1b", value: "llama3.2:1b", size: Memory.to_bytes(1.3, :GB)],
-    [key: "Phi4 14b", value: "phi4", size: Memory.to_bytes(9.1, :GB)],
-    [key: "Llama 3.1 8b", value: "llama3.1:8b", size: Memory.to_bytes(4.9, :GB)],
-    [key: "Llama 3.1 70b", value: "llama3.1:70b", size: Memory.to_bytes(43.0, :GB)],
-    [key: "Nomic embed-text 137m", value: "nomic-embed-text", size: Memory.to_bytes(274, :MB)],
-    [key: "mxbai embed [Large] 335m", value: "mxbai-embed-large", size: Memory.to_bytes(670, :MB)],
-    [key: "Snowflake Arctic Embed 2.0 568m", value: "snowflake-arctic-embed2", size: Memory.to_bytes(1.2, :GB)]
-  ]
+  @models %{
+    "deepseek-r1:1.5b" => %{name: "Deepseek R1 1.5b", size: Memory.to_bytes(1.1, :GB)},
+    "deepseek-r1:7b" => %{name: "Deepseek R1 7b", size: Memory.to_bytes(4.7, :GB)},
+    "llama3.2:3b" => %{name: "Llama 3.2 3b", size: Memory.to_bytes(2.0, :GB)},
+    "llama3.2:1b" => %{name: "Llama 3.2 1b", size: Memory.to_bytes(1.3, :GB)},
+    "phi4" => %{name: "Phi4 14b", size: Memory.to_bytes(9.1, :GB)},
+    "llama3.1:8b" => %{name: "Llama 3.1 8b", size: Memory.to_bytes(4.9, :GB)},
+    "llama3.1:70b" => %{name: "Llama 3.1 70b", size: Memory.to_bytes(43.0, :GB)},
+    "nomic-embed-text" => %{name: "Nomic embed-text 137m", size: Memory.to_bytes(274, :MB)},
+    "mxbai-embed-large" => %{name: "mxbai embed [Large] 335m", size: Memory.to_bytes(670, :MB)},
+    "snowflake-arctic-embed2" => %{name: "Snowflake Arctic Embed 2.0 568m", size: Memory.to_bytes(1.2, :GB)}
+  }
 
   @derive {
     Flop.Schema,
@@ -61,7 +61,7 @@ defmodule CommonCore.Ollama.ModelInstance do
   batt_schema "model_instances" do
     slug_field :name
 
-    field :model, :string, default: "llama3.1"
+    field :model, :string, default: "llama3.2:1b"
     field :num_instances, :integer, default: 1
 
     field :cpu_requested, :integer
@@ -83,6 +83,7 @@ defmodule CommonCore.Ollama.ModelInstance do
     model_instance
     |> CommonCore.Ecto.Schema.schema_changeset(attrs, opts)
     |> maybe_set_virtual_size(@presets)
+    |> validate_inclusion(:model, Map.keys(@models))
     |> validate_number(:cpu_requested, greater_than: 0, less_than: 100_000)
     |> validate_number(:cpu_limits, greater_than: 0, less_than: 100_000)
     |> validate_number(:memory_requested, greater_than: 0, less_than: Memory.to_bytes(1_000, :TB))
@@ -90,22 +91,19 @@ defmodule CommonCore.Ollama.ModelInstance do
     |> foreign_key_constraint(:project_id)
   end
 
-  def preset_options_for_select do
-    Enum.map(@presets, &{String.capitalize(&1.name), &1.name})
-  end
-
   def preset_options_for_select(model_name) do
-    size =
-      Enum.reduce_while(@models, 0, fn mdl, acc ->
-        if Keyword.get(mdl, :value) == model_name, do: {:halt, Keyword.get(mdl, :size)}, else: {:cont, acc}
-      end)
+    size = model_size(model_name)
 
     Enum.map(@presets, &[key: String.capitalize(&1.name), value: &1.name, disabled: &1.memory_requested < size])
   end
 
   def model_options_for_select do
-    Enum.map(@models, fn [key: key, value: value, size: size] ->
-      [key: "#{key} (#{Memory.humanize(size, false)})", value: value]
+    # the key in the @model registry is what should/would be stored in the db
+    # the key for the form is what the user chooses
+    Enum.map(@models, fn {key, %{name: name, size: size}} ->
+      [key: "#{name} (#{Memory.humanize(size, false)})", value: key]
     end)
   end
+
+  defp model_size(model_name), do: @models |> Map.get(model_name, %{}) |> Map.get(:size, 0)
 end
