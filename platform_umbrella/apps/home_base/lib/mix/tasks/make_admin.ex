6fd7a406fc9a708daea966ae95456503ed06f5c3
Elliott Clark <elliott@batteriesincl.com>
2025-03-04 18:41:24 -0600

    fix: make dialyzer happy

--- platform_umbrella/apps/home_base/lib/mix/tasks/make_admin.ex
+++ platform_umbrella/apps/home_base/lib/mix/tasks/make_admin.ex
@@ -7,18 +7,23 @@ defmodule Mix.Tasks.HomeBase.MakeAdmin do
   use Mix.Task
 
   @start_apps [:postgrex, :ecto, :ecto_sql, :home_base]
-  @shutdown {:shutdown, 1}
+  @shutdown_adnormal {:shutdown, 1}
+  @shutdown_normal {:shutdown, 0}
+
+  # Run is always going to exit.
+  # That's expected
+  @dialyzer {:nowarn_function, run: 1}
 
   def run(args) do
     case args do
       [email] ->
         {:ok, _} = Application.ensure_all_started(@start_apps)
         HomeBase.Release.make_admin!(email)
-        exit(@shutdown)
+        exit(@shutdown_normal)
 
       _ ->
         Mix.shell().error("Error: Please pass a user email as the first argument")
-        exit(@shutdown)
+        exit(@shutdown_adnormal)
     end
   end
 end
