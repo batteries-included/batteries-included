19d90fa6da6275a5f175a7f7a52aa8363d188ded
Elliott <elliott@batteriesincl.com>
2025-03-25 10:38:23 -0600

    chore: prepare for 0.58.0 release (#1757)
    Summary:
    Next release incoming
    
    Test Plan:
    - CI

--- platform_umbrella/apps/home_base_web/mix.exs
+++ platform_umbrella/apps/home_base_web/mix.exs
@@ -4,7 +4,7 @@ defmodule HomeBaseWeb.MixProject do
   def project do
     [
       app: :home_base_web,
-      version: "0.57.0",
+      version: "0.58.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
