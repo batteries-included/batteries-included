94447b4cf18d971b62959b229aedc679f64ea566
Elliott Clark <elliott@batteriesincl.com>
2025-04-01 09:28:43 -0500

    chore: prepare for 0.60.0
    Summary:
    - Release 0.59.0 to fix secure production
    
    Test Plan:
    - CI

--- platform_umbrella/apps/kube_bootstrap/mix.exs
+++ platform_umbrella/apps/kube_bootstrap/mix.exs
@@ -4,7 +4,7 @@ defmodule KubeBootstrap.MixProject do
   def project do
     [
       app: :kube_bootstrap,
-      version: "0.59.0",
+      version: "0.60.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
