f1a9489139c60a8dfe356b8d1f77475012067823
Jason Tarasovic <JTarasovic@users.noreply.github.com>
2025-04-09 12:42:46 -0500

    chore: refactor json logging configuration (#1851)
    Move the formatter configuration to runtime config. Moving to `new/1`
    should ensure that we always have a valid formatter config. I think the
    issue we had was that `redactors` was `nil` instead of `[]` which I
    fixed (temporarily) by copying the output of new to the compile time
    config.
    
    Co-authored-by: Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>

--- platform_umbrella/config/runtime.exs
+++ platform_umbrella/config/runtime.exs
@@ -3,3 +3,7 @@ import Config
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   level: :debug
+
+if config_env() == :prod do
+  config :logger, :default_handler, formatter: LoggerJSON.Formatters.Basic.new(metadata: [:mfa, :request_id])
+end
