05efbf5d9fa6cff08e2290911c7aeb209b67d40f
Elliott Clark <elliott@batteriesincl.com>
2025-04-14 10:41:11 -0500

    chore: prepare for 0.66.0 development
    Summary:
    - bix set-version 0.66.0
    
    Test Plan:
    - Release after the bi version bump lands

--- platform_umbrella/apps/kube_bootstrap/mix.exs
+++ platform_umbrella/apps/kube_bootstrap/mix.exs
@@ -4,7 +4,7 @@ defmodule KubeBootstrap.MixProject do
   def project do
     [
       app: :kube_bootstrap,
-      version: "0.65.0",
+      version: "0.66.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
