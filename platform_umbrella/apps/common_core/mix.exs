2a6970d93d84b9551113d9f47da74b37ec2b8bae
Elliott Clark <elliott@batteriesincl.com>
2025-04-21 15:24:22 -0500

    chore: prepare for 0.67.0
    Summary:
    - bix set-version
    - This will land after the bi bump and the images are updated.
    
    Test Plan:
    - CI
    - I'll cut a release assuming the verify is clean

--- platform_umbrella/apps/common_core/mix.exs
+++ platform_umbrella/apps/common_core/mix.exs
@@ -4,7 +4,7 @@ defmodule CommonCore.MixProject do
   def project do
     [
       app: :common_core,
-      version: "0.66.0",
+      version: "0.67.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- platform_umbrella/apps/control_server/mix.exs
+++ platform_umbrella/apps/control_server/mix.exs
@@ -4,7 +4,7 @@ defmodule ControlServer.MixProject do
   def project do
     [
       app: :control_server,
-      version: "0.66.0",
+      version: "0.67.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
