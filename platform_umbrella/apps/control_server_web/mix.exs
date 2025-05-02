665b8802833b7a78704a83b1917c2893892319be
Elliott <elliott@batteriesincl.com>
2025-05-02 16:34:45 -0500

    chore: prepare for 0.69.0 development (#1959)
    Summary:
    Niiiice
    
    Test Plan:
    CI

--- platform_umbrella/apps/control_server_web/mix.exs
+++ platform_umbrella/apps/control_server_web/mix.exs
@@ -4,7 +4,7 @@ defmodule ControlServerWeb.MixProject do
   def project do
     [
       app: :control_server_web,
-      version: "0.68.0",
+      version: "0.69.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
