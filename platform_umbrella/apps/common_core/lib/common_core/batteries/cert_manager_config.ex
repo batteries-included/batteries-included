a8d89121e470096b4f8134cab6c4f2448e637522
Jason Tarasovic <JTarasovic@users.noreply.github.com>
2025-05-09 12:34:13 -0500

    chore: remove unused cert manager image (#1987)
    The `cert_manager_ctl` image went away after 1.14.x and we haven't used
    it since then either.
    
    Co-authored-by: Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>

--- platform_umbrella/apps/common_core/lib/common_core/batteries/cert_manager_config.ex
+++ platform_umbrella/apps/common_core/lib/common_core/batteries/cert_manager_config.ex
@@ -3,13 +3,21 @@ defmodule CommonCore.Batteries.CertManagerConfig do
 
   use CommonCore, :embedded_schema
 
+  alias CommonCore.Ecto.Schema
+
   batt_polymorphic_schema type: :cert_manager do
     defaultable_image_field :acmesolver_image, image_id: :cert_manager_acmesolver
     defaultable_image_field :cainjector_image, image_id: :cert_manager_cainjector
     defaultable_image_field :controller_image, image_id: :cert_manager_controller
-    defaultable_image_field :ctl_image, image_id: :cert_manager_ctl
     defaultable_image_field :webhook_image, image_id: :cert_manager_webhook
 
     field :email, :string
+  end
+
+  @ctl_keys_to_drop ~w(ctl_image ctl_image_name_override ctl_image_tag_override)a
+  def load(data) do
+    data
+    |> Map.drop(@ctl_keys_to_drop ++ Enum.map(@ctl_keys_to_drop, &Atom.to_string/1))
+    |> then(&Schema.schema_load(__MODULE__, &1))
   end
 end
