8a5f410ae73fcf2f673235e413944c6f86c4d8ff
Elliott Clark <elliott@batteriesincl.com>
2025-05-12 23:42:11 -0500

    feat: add `list_snapshots/1` to `KubeServices.ET.HomeBaseClient`
    Description:
    - Add a `list_snapshots` method
    - Change `HomeBase.Projects.snapshots_for` to be suitable for larger
      lists. (Don't pass all the project details)
    - Change displays to use the new structure
    - Add the start of `ImportForm` that will be used to import projects
      while creating a new one
    
    Test Plan:
    - tests changed

--- platform_umbrella/apps/home_base_web/lib/home_base_web/controllers/projects/stored_project_snapshot_controller.ex
+++ platform_umbrella/apps/home_base_web/lib/home_base_web/controllers/projects/stored_project_snapshot_controller.ex
@@ -12,14 +12,13 @@ defmodule HomeBaseWeb.StoredProjectSnapshotController do
   action_fallback HomeBaseWeb.FallbackController
 
   def create(conn, %{"installation_id" => install_id, "jwt" => jwt}) do
-    installation = CustomerInstalls.get_installation!(install_id)
-    snapshot = CommonCore.JWK.decrypt_from_control_server!(installation.control_jwk, jwt)
-
-    with {:ok, %StoredProjectSnapshot{} = stored_project_snapshot} <-
-           Projects.create_stored_project_snapshot(%{snapshot: snapshot, installation_id: install_id}) do
+    with %{} = installation <- CustomerInstalls.get_installation!(install_id),
+         snapshot = CommonCore.JWK.decrypt_from_control_server!(installation.control_jwk, jwt),
+         {:ok, %StoredProjectSnapshot{} = stored_project_snapshot} <-
+           Projects.create_stored_project_snapshot(%{snapshot: snapshot, installation_id: installation.id}) do
       conn
       |> put_status(:created)
-      |> render(:show, stored_project_snapshot: stored_project_snapshot)
+      |> render(:show, payload: stored_project_snapshot)
     end
   end
 
@@ -30,6 +29,18 @@ defmodule HomeBaseWeb.StoredProjectSnapshotController do
 
     conn
     |> put_status(:ok)
-    |> render(:index, payload: CommonCore.JWK.encrypt_to_control_server(installation.control_jwk, payload))
+    |> put_view(json: HomeBaseWeb.JwtJSON)
+    |> render(:show, jwt: CommonCore.JWK.encrypt_to_control_server(installation.control_jwk, payload))
+  end
+
+  def show(conn, %{"installation_id" => install_id, "id" => id}) do
+    with %{} = installation <- CustomerInstalls.get_installation!(install_id),
+         {:ok, %StoredProjectSnapshot{} = stored_project_snapshot} <-
+           Projects.get_stored_project_snapshot(installation, id) do
+      conn
+      |> put_status(:ok)
+      |> put_view(json: HomeBaseWeb.JwtJSON)
+      |> render(:show, jwt: CommonCore.JWK.encrypt_to_control_server(installation.control_jwk, stored_project_snapshot))
+    end
   end
 end
