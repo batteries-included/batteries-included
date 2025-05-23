2f7fbd219b1f7e23094c16fd24467f934602f14f
Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>
2025-05-23 10:28:41 -0500

    feat(verify): add net_sec tests

---
+++ platform_umbrella/apps/verify/test/kiali_test.exs
@@ -0,0 +1,12 @@
+defmodule Verify.KialiTest do
+  use Verify.TestCase, async: false, batteries: ~w(kiali)a
+
+  verify "kiali is running", %{session: session} do
+    session
+    |> assert_pod_running("kiali-")
+    |> visit("/net_sec")
+    |> click_external(Query.css("a", text: "Kiali"))
+    # find the link to the website in the footer
+    |> assert_has(Query.css(~s|header img[alt="Kiali Logo"]|))
+  end
+end
