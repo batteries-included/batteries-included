dd67cef75d01b7bad69d1685064996a68ef54a60
Jason Tarasovic <JTarasovic@users.noreply.github.com>
2025-05-23 08:41:18 -0500

    feat(dev): recompile on registry change (#2025)

--- platform_umbrella/apps/common_core/lib/common_core/defaults/registry.ex
+++ platform_umbrella/apps/common_core/lib/common_core/defaults/registry.ex
@@ -4,14 +4,22 @@ defmodule CommonCore.Defaults.Registry do
   alias CommonCore.Defaults.Image
 
   defmacro __before_compile__(_env) do
+    registry_file = Path.relative_to_cwd("../../../image_registry.yaml")
+    registry_hash = registry_file |> File.read!() |> :erlang.md5()
+
     registry =
-      "../../../image_registry.yaml"
-      |> Path.relative_to_cwd()
+      registry_file
       |> YamlElixir.read_from_file!()
       |> Map.new(fn {key, val} -> {String.to_atom(key), Image.new!(val)} end)
 
     quote do
+      # elixir will recompile this module if this file changes
+      @external_resource unquote(registry_file)
+
       def registry, do: unquote(Macro.escape(registry))
+
+      # we'll also recompile if the hash changes
+      def __mix_recompile__?, do: unquote(registry_file) |> File.read!() |> :erlang.md5() !== unquote(registry_hash)
     end
   end
 end
