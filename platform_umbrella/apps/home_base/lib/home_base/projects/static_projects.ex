6da275068d6dba96ac07225ec416fd69cb2a78a0
Elliott Clark <elliott@batteriesincl.com>
2025-06-06 10:41:50 -0500

    feat: add new traditional example project
    Description:
    Knative is a beast sometimes. So offer an example that doesn't use it.
    
    Test Plan:
    Import shows it.

--- platform_umbrella/apps/home_base/lib/home_base/projects/static_projects.ex
+++ platform_umbrella/apps/home_base/lib/home_base/projects/static_projects.ex
@@ -2,11 +2,16 @@ defmodule HomeBase.Projects.StaticProjects do
   @moduledoc false
   use CommonCore.IncludeResource,
     rag_v0: "priv/stored_projects/batt_0197313f7c447c85ab2462d284f01077.json",
-    serverless_v0: "priv/stored_projects/batt_019741bce3157609b800f611baa82b9a.json"
+    serverless_web_v0: "priv/stored_projects/batt_019741bce3157609b800f611baa82b9a.json",
+    traditional_web_v0: "priv/stored_projects/batt_019745dfea447de6ae4b47b7bb7834e4.json"
 
   def static_projects(install_id \\ nil) do
     Map.new(
-      [{"batt_0197313f7c447c85ab2462d284f01077", :rag_v0}, {"batt_019741bce3157609b800f611baa82b9a", :serverless_v0}],
+      [
+        {"batt_0197313f7c447c85ab2462d284f01077", :rag_v0},
+        {"batt_019741bce3157609b800f611baa82b9a", :serverless_web_v0},
+        {"batt_019745dfea447de6ae4b47b7bb7834e4", :traditional_web_v0}
+      ],
       fn {id, resource} ->
         {id, to_stored(id, to_snapshot(resource, install_id))}
       end
