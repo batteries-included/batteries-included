03c3f0e3dbd23138d1eda557429f1d25f3417003
Elliott <elliott@batteriesincl.com>
2025-07-01 11:07:52 -0500

    chore: bump stable bi version (#2266)
    Description:
    This should be part of bix s set-version by now
    
    Test Plan:
    - CI

--- platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
+++ platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
@@ -2,5 +2,5 @@ defmodule CommonCore.Defaults.Versions do
   @moduledoc false
 
   @spec bi_stable_version() :: String.t()
-  def bi_stable_version, do: "0.72.0"
+  def bi_stable_version, do: "0.73.0"
 end
