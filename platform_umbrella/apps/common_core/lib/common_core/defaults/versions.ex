959a18a1ed9ff1e2ddf57322908106dc59f83bf7
Elliott Clark <elliott@batteriesincl.com>
2025-07-02 14:53:21 -0500

    feat: bix s set-version should bump stable bi when possibl
    Description:
    If the stable version of bi was the last release (somone hasn't needed
    to pin the version) then we should advance the stable bi version when
    setting a new version
    
    Test Plan:
    - Tried it locally.

--- platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
+++ platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
@@ -1,6 +1,8 @@
 defmodule CommonCore.Defaults.Versions do
   @moduledoc false
 
+  @stable_version "0.74.0"
+
   @spec bi_stable_version() :: String.t()
-  def bi_stable_version, do: "0.74.0"
+  def bi_stable_version, do: @stable_version
 end
