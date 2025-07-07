b31b3e57c7dfd8f5756e882fd000870416f7a2da
Elliott Clark <elliott@batteriesincl.com>
2025-07-07 10:06:34 -0500

    style: add tailwindcss to keycloak-theme
    Summary:
    - CSS styles though not used yet
    
    Test Plan:
    - Visual when I get the chance

--- keycloak-theme/vite.config.ts
+++ keycloak-theme/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { keycloakify } from 'keycloakify/vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     keycloakify({
       accountThemeImplementation: 'none',
