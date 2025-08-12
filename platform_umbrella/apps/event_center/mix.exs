a226c8b4584cd33529cce179bf84d65c903b9a4a
Elliott <elliott@batteriesincl.com>
2025-08-12 14:23:37 -0500

    chore: prepare for 1.0.2 (#2457)
    Summary:
    - Hot fix that mother
    
    Test Plan:
    - CI

--- platform_umbrella/apps/event_center/mix.exs
+++ platform_umbrella/apps/event_center/mix.exs
@@ -4,7 +4,7 @@ defmodule EventCenter.MixProject do
   def project do
     [
       app: :event_center,
-      version: "1.0.1",
+      version: "1.0.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- platform_umbrella/apps/verify/mix.exs
+++ platform_umbrella/apps/verify/mix.exs
@@ -4,7 +4,7 @@ defmodule Verify.MixProject do
   def project do
     [
       app: :verify,
-      version: "1.0.1",
+      version: "1.0.2",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
