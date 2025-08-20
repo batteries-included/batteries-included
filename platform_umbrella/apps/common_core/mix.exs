3dc7cd89c5481f524b5a9d7bce8e7b969cfe8c95
Elliott <elliott@batteriesincl.com>
2025-08-20 15:28:45 -0500

    chore: prepare for 1.2.0 development (#2495)
    Summary:
    - bix s set-version 1.2.0
    
    Test Plan:
    - ci

--- platform_umbrella/apps/common_core/mix.exs
+++ platform_umbrella/apps/common_core/mix.exs
@@ -4,7 +4,7 @@ defmodule CommonCore.MixProject do
   def project do
     [
       app: :common_core,
-      version: "1.1.0",
+      version: "1.2.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
