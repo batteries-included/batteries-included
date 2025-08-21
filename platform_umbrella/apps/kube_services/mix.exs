2e2442ceeaeaab0aadf283cf38ba77455495075f
Elliott Clark <elliott@batteriesincl.com>
2025-08-21 09:41:23 -0500

    chore: Revert "chore: prepare for 1.3.0 (#2499)"
    This reverts commit 6444a07b870244e647fca33af347cfcfcc010145.
    
    When I was planning on stamping a 1.2.0 quickly, I thought the tests
    were just flakey. I think they are broken, and we can't currently run
    two batteries included clusters on the same machine.

--- platform_umbrella/apps/kube_services/mix.exs
+++ platform_umbrella/apps/kube_services/mix.exs
@@ -4,7 +4,7 @@ defmodule KubeServices.MixProject do
   def project do
     [
       app: :kube_services,
-      version: "1.3.0",
+      version: "1.2.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- platform_umbrella/mix.exs
+++ platform_umbrella/mix.exs
@@ -4,7 +4,7 @@ defmodule ControlServer.Umbrella.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "1.3.0",
+      version: "1.2.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: releases(),
