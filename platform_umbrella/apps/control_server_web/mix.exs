f7337186e3f0386f2819074240c6523ddb668862
Elliott Clark <elliott@batteriesincl.com>
2025-09-29 13:31:19 -0500

    chore: prepare for 1.8.0

--- platform_umbrella/apps/control_server_web/mix.exs
+++ platform_umbrella/apps/control_server_web/mix.exs
@@ -4,7 +4,7 @@ defmodule ControlServerWeb.MixProject do
   def project do
     [
       app: :control_server_web,
-      version: "1.7.0",
+      version: "1.8.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
