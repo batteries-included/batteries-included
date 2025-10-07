40aa83009a6ca7718d31e8a9465c2003448c5662
Elliott Clark <elliott@batteriesincl.com>
2025-10-07 10:13:06 -0500

    chore: prepare for 1.9.0 development
    Summary:
    - bix s set-version 1.9.0
    
    Test Plan:
    - CI

--- platform_umbrella/apps/kube_bootstrap/mix.exs
+++ platform_umbrella/apps/kube_bootstrap/mix.exs
@@ -4,7 +4,7 @@ defmodule KubeBootstrap.MixProject do
   def project do
     [
       app: :kube_bootstrap,
-      version: "1.8.0",
+      version: "1.9.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
