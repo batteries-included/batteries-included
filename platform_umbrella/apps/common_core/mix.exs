9828b8c877319fa5b93d22bc9023887b8467c52f
Jason Tarasovic <2999202+JTarasovic@users.noreply.github.com>
2025-10-09 11:32:01 -0500

    chore: prepare for 1.10.0

--- platform_umbrella/apps/common_core/mix.exs
+++ platform_umbrella/apps/common_core/mix.exs
@@ -4,7 +4,7 @@ defmodule CommonCore.MixProject do
   def project do
     [
       app: :common_core,
-      version: "1.9.0",
+      version: "1.10.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
