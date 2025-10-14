bb17c57d40c0bcc263c9262df91524e621ab6f35
Elliott <elliott@batteriesincl.com>
2025-10-14 10:17:09 -0500

    chore: prepare for 1.11.0 development (#2770)
    Summary:
    bix s set-version
    
    Test Plan:
    ci

--- platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
+++ platform_umbrella/apps/common_core/lib/common_core/defaults/versions.ex
@@ -1,7 +1,7 @@
 defmodule CommonCore.Defaults.Versions do
   @moduledoc false
 
-  @stable_version "1.10.0"
+  @stable_version "1.11.0"
 
   @spec bi_stable_version() :: String.t()
   def bi_stable_version, do: @stable_version
--- platform_umbrella/apps/kube_services/mix.exs
+++ platform_umbrella/apps/kube_services/mix.exs
@@ -4,7 +4,7 @@ defmodule KubeServices.MixProject do
   def project do
     [
       app: :kube_services,
-      version: "1.10.0",
+      version: "1.11.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
