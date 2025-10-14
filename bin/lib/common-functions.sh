f9fef070e451071fe299fb2ed0d306ca4bbe7c29
Elliott Clark <elliott@batteriesincl.com>
2025-10-14 13:43:26 -0500

    chore: prepare for 1.12.0
    Summary:
    Release the removal of pdbs to scale down nodes
    
    Test Plan:
    CI

--- bin/lib/common-functions.sh
+++ bin/lib/common-functions.sh
@@ -4,7 +4,7 @@ TRACE=${TRACE-""}
 
 # If you are changing this also change the
 # version in mix.exs for all the apps in platform_umbrella
-export BASE_VERSION="1.11.0"
+export BASE_VERSION="1.12.0"
 
 export REGISTRY="ghcr.io/batteries-included"
 
--- platform_umbrella/apps/home_base_web/mix.exs
+++ platform_umbrella/apps/home_base_web/mix.exs
@@ -4,7 +4,7 @@ defmodule HomeBaseWeb.MixProject do
   def project do
     [
       app: :home_base_web,
-      version: "1.11.0",
+      version: "1.12.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
