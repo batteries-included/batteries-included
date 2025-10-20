4eac212de3e4542d730292c8e20ce486bca8963e
Elliott Clark <elliott@batteriesincl.com>
2025-10-20 09:16:39 -0500

    chore: prepare for 1.13.0 development
    Summary:
    - Release with home base
    
    Test Plan:
    - CI

--- platform_umbrella/apps/control_server/mix.exs
+++ platform_umbrella/apps/control_server/mix.exs
@@ -4,7 +4,7 @@ defmodule ControlServer.MixProject do
   def project do
     [
       app: :control_server,
-      version: "1.12.0",
+      version: "1.13.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
